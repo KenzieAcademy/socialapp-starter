60bd62715ba01f02fcd4290e7ced24a679db7e62
william_michael_brough <eatmyloudmusic@gmail.com>
2020-09-08 17:12:47 -0400

    Coded Registration and DataServices dot JS

---
+++ src/components/registrationForm/DataService.js
@@ -0,0 +1,16 @@
+//import the axios HTTP client to communicate with the API
+import axios from 'axios';
+class DataService {
+    constructor(url = 'https://socialapp-api.herokuapp.com/', client = axios.create()){
+        this.url = url;
+        this.client = client;
+    }
+    registerUser(userData){
+        return this.client.get(this.url + "/users", userData);
+    }
+
+    getUsers(){
+      return this.client.get(this.url + "/users")
+    }
+}
+export default DataService;
---
+++ src/components/registrationForm/RegistrationForm.js
@@ -0,0 +1,62 @@
+import React from "react";
+import Spinner from "react-spinkit";
+import { withAsyncAction } from "../../redux/HOCs";
+import "./RegistrationForm.css";
+import DataService from "./DataService";
+
+
+
+class RegistrationForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: "",
+      password: "",
+      displayName: ""
+    };
+    this.client = new DataService();
+  }
+
+  handleRegistration = e => {
+    e.preventDefault();
+    this.client.registrationUser(this.state).then(result =>{
+      alert(JSON.stringify(result.data))
+    });
+  };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    const { loading, error } = this.props;
+    return (
+      <div className="RegistrationForm">
+        <form id="registration-form" onSubmit={this.handleRegistration}>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            name="username"
+            autoFocus
+            required
+            onChange={this.handleChange}
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            required
+            onChange={this.handleChange}
+          />
+          <button type="submit" disabled={loading}>
+            Registration
+          </button>
+        </form>
+        {loading && <Spinner name="circle" color="blue" />}
+        {error && <p style={{ color: "red" }}>{error.message}</p>}
+      </div>
+    );
+  }
+}
+
+export default RegistrationForm;
