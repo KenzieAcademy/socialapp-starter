8f55651ca3c27c78acbf4fd8b91f8eedacfcc3d5
Samantha Taylor <staylor@mort.is>
2020-09-08 14:25:15 -0400

    Started the MessageFeed and changed the Menu to be shown on all pages.

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -12,10 +12,10 @@ class Menu extends React.Component {
   render() {
     return (
       <div className="Menu">
-        <h1>Kwitter</h1>
+        <h1>The QuestBoard</h1>
         {this.props.isAuthenticated && (
           <div id="menu-links">
-            <Link to="/messagefeed">Message Feed</Link>
+            <Link to="/MessageFeed">Message Feed</Link>
             <Link to="/" onClick={this.handleLogout}>
               Logout
             </Link>
