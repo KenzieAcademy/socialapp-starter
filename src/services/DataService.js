9d934ec25a6b1f3f5631b58583360599d9d5e84b
Chuefeng Vue <chuefe.vue@gmail.com>
2020-09-10 13:49:21 -0500

    DataService.js File Revision due to function outside of class

--- src/services/DataService.js
+++ src/services/DataService.js
@@ -1,7 +1,6 @@
 //import the axios HTTP client to communicate with the API
 import axios from "axios";
 class DataService {
-    
   constructor(
     url = "https://socialapp-api.herokuapp.com",
     client = axios.create()
@@ -9,17 +8,22 @@ class DataService {
     this.url = url;
     this.client = client;
   }
-  
+
   registerUser(registrationData) {
     return this.client.post(this.url + "/users", registrationData);
   }
-}
-// getMessages
-//
-//
+  // getMessages
+  //
+  //
 
-postMessage(message){
-    let loginData = JSON.parse(localStorage.getItem("login"))
-    return this.client.post(this.url + "/messages", message, { headers: {Authorization: `Bearer ${loginData.result.token}`}})
+  postMessage(message) {
+    let loginData = JSON.parse(localStorage.getItem("login"));
+
+    return this.client.post(this.url + "/messages", message, {
+      headers: {
+        Authorization: `Bearer ${loginData.result.token}`,
+      },
+    });
+  }
 }
 export default DataService;
