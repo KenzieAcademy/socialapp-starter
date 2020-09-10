2375b471d047d7c77cb4aae9ecf2ec7bf3c55fcc
Your NameAlicia <alicialwilliamsnyc@gmail.com>
2020-09-10 16:08:02 -0400

    committing-changes-for-merge

---
+++ src/DataService.js
@@ -0,0 +1,10 @@
+//import the axios HTTP client to communicate with the API
+import axios from axios;
+class DataService {
+    constructor(url = 'https://socialapp-api.herokuapp.com', client = axios.create()){
+    this.url = url;
+    this.client = client;
+}
+get 
+
+}
