7f5e5e8f7ac91e03519060e60bc46cc492067155
Chuefeng Vue <chuefe.vue@gmail.com>
2020-09-10 16:22:47 -0500

    Working with Code Here

---
+++ src/components/getMessages/GetMessages.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Spinner from "react-spinkit";
+import dataService from "../../services/DataService";
+
+class GetMessages extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    <div>{/* Messages */}</div>;
+  }
+}
