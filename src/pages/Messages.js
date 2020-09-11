9ed435f486c60882a45e518089b7a3396ea078fb
Sir Rodriguez <dr.raw.phd@gmail.com>
2020-09-11 15:02:29 -0400

    added messages page/routing

---
+++ src/pages/Messages.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Messages extends Component {
+  render() {
+    return (
+      <div className="MessagesDiv">
+        <h1>Our Messages go on this page</h1>
+      </div>
+    );
+  }
+}
+
+export default Messages;
