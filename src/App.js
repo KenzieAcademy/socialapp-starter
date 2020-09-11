33fe9a6f838fb5cb4601293953cb056e256ddb3e
gregory harris <gregoryharris@gregorys-mbp.attlocal.net>
2020-09-11 15:34:15 -0500

    Add route for feed message

--- src/App.js
+++ src/App.js
@@ -5,25 +5,18 @@ import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
 
+
+
+import PostMessageForm from "./components/postmessageform/PostMessageForm";
+
 class App extends React.Component {
   render() {
     return (
       <Switch>
-        <Route
-          exact
-          path="/"
-          component={Home}
-        />
-        <Route
-          exact
-          path="/profile/:username"
-          component={Profile}
-        />
-        <Route
-          exact
-          path="*"
-          component={NotFound}
-        />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/profile/:username" component={Profile} />
+        <Route exact path="/messagefeed" component={PostMessageForm} />
+        <Route exact path="*" component={NotFound} />
       </Switch>
     );
   }
