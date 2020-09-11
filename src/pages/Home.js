0c19bbc661f8941669f478ff28e5989263bcaf2d
foko53 <jenfoko@gmail.com>
2020-09-11 14:51:31 -0400

    Input box

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,6 @@ import React from "react";
 import LoginForm from "../components/loginForm/LoginForm";
 import Menu from "../components/menu/Menu";
 import { userIsNotAuthenticated } from "../redux/HOCs";
-import LogoutButton from "../components/logoutButton/LogoutButton";
-import DeleteMessage from "../components/deleteMessage/DeleteMessage";
-import UploadPhoto from "../components/uploadPhoto/UploadPhoto";
 
 class Home extends React.Component {
   render() {
@@ -13,9 +10,6 @@ class Home extends React.Component {
         <Menu />
         <h2>Your favorite microblogging platform</h2>
         <LoginForm />
-        <LogoutButton />
-        <DeleteMessage />
-        <UploadPhoto />
       </div>
     );
   }
