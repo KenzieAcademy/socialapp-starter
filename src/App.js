0decc6b9cb246617f492766df32e2992f31bd2c7
Ben Lynch <blynchisonfire@gmail.com>
2020-09-11 17:51:01 -0400

    Prettied up the home screen and got login/register forms switching

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
@@ -9,21 +10,9 @@ class App extends React.Component {
   render() {
     return (
       <Switch>
-        <Route
-          exact
-          path="/"
-          component={Home}
-        />
-        <Route
-          exact
-          path="/profile/:username"
-          component={Profile}
-        />
-        <Route
-          exact
-          path="*"
-          component={NotFound}
-        />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/profile/:username" component={Profile} />
+        <Route exact path="*" component={NotFound} />
       </Switch>
     );
   }
