59c918d608a86855befb5dd23245ef787d82139c
Amisha <pamisha22@yahoo.com>
2020-09-12 15:27:38 -0400

    Adding counts to like button

--- src/components/likebutton/LikeButton.js
+++ src/components/likebutton/LikeButton.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "./likebutton.css";
 class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messageID: 0,
+      count: 0,
+    };
+  }
+
+  incrementMe = () => {
+    let newCount = this.state.count + 1;
+    this.setState({
+      count: newCount,
+    });
+  };
   render() {
     return (
       <div className="LikeButton">
-        <button id="like">Like</button>
+        <button id="like" onClick={this.incrementMe}>
+          Like {this.state.count}
+        </button>
       </div>
     );
   }
