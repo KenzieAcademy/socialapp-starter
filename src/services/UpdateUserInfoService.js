f807a608fad935524f8bf057858000b780792175
Nick D <nddudash@fouytan.local>
2020-09-13 17:05:35 -0600

    Created Update User Info Form and related service. Also refactored all other forms under Semantic UI

---
+++ src/services/UpdateUserInfoService.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+class UpdateUserInfoService {
+  constructor(
+    url = "https://socialapp-api.herokuapp.com",
+    client = axios.create()
+  ) {
+    this.url = url;
+    this.client = client;
+  }
+
+  updateInfo(updateInfo) {
+    let loginData = JSON.parse(localStorage.getItem("login"));
+
+    return this.client.patch(
+      this.url + "/users/" + `${loginData.result.username}`,
+      updateInfo,
+      {
+        headers: {
+          Authorization: `Bearer ${loginData.result.token}`,
+        },
+      }
+    );
+  }
+}
+
+export default UpdateUserInfoService;
