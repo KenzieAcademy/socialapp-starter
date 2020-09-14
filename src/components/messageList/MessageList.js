ab6952d507104f4244a6c19674758ee9e99d9a6f
Doris Mondo <doris.ekwe@yahoo.com>
2020-09-14 16:12:08 -0400

    update

--- src/components/messageList/MessageList.js
+++ src/components/messageList/MessageList.js
@@ -1,19 +1,18 @@
 import React from 'react'
-
+import Message from '../message/Messages'
 import './MessageList.css'
 
 function MessageList(props) {
-    const listItems = props.message.map((message, i) =>
-        <Messages key={i}>{messages.read}({messages.unread}>))
+    const listItems = props.message.map((message, i) => <Message key={i} message={message} />
+      
+return (
+        <ul className="Message">
+            {listItems}
+        </ul>
+
+
+
+    )
 }
 
- <button>Delete</button>
-return (
-    <ul className="Message">
-                {listItems}
-            </ul>
-           
-    
-
-)
 export default MessageList
