7eb3771484d37bfb8274c83e8f2f28abc38f9842
Javiere <snzjavi@gmail.com>
2020-09-14 09:17:39 -0500

    test

--- src/App.js
+++ src/App.js
@@ -4,21 +4,41 @@ import axios from "axios"
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
+import {
+
+} from 'semantic-ui-react';
+
 
 class App extends React.Component {
-  componentDidMount() {
-    axios.get("https://socialapp-api.herokuapp.com/docs/")
-    .then(response => {
-      console.log(response.data)
-    })
-    .catch(error => {
-      console.log(error)
-    })
-  }
+
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     name: null,
+  //     email: null
+  //   }
+  // }
+
+  // componentDidMount() {
+  //   axios.get("https://socialapp-api.herokuapp.com/docs/users")
+  //   .then(response => {
+  //     this.setState({
+  //       name: response.data.user,
+  //       // email: response.data.email
+  //     })
+  //   })
+  //   .catch(error => {
+  //     console.log(error)
+  //   })
+  // }
 
   render() {
     return (
       <Switch>
+      {/* <div>
+      <span>{this.state.user}</span>
+    </div> */}
+
         <Route
           exact
           path="/"
