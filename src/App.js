236d48e39352376915d150105bb3f6e53aa46d96
James Farfan <jamesrojas710@gmail.com>
2020-09-14 15:10:23 -0400

    no changes made in profile.js -just testing.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
-import Messagefeed from "./pages/Messagefeed";
+import Message from "./pages/Messagefeed";
 
 class App extends React.Component {
   render() {
@@ -28,8 +28,8 @@ class App extends React.Component {
 
         <Route
           exact
-          path="Messagefeed"
-          component={Messagefeed}
+          path="/Messagefeed"
+          component={Message}
         />
       </Switch>
     );
