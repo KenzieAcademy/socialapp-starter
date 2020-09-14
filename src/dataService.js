dfd0ee5d4af23bf7ba11b51c844f8ce4d3aa14d8
Lesa Ducksworth <lducks67@gmail.com>
2020-09-14 16:23:04 -0500

    updated Registration component, started work on UsersList component

--- src/dataService.js
+++ src/dataService.js
@@ -2,13 +2,19 @@
 import axios from 'axios';
 
 class DataService {
-    constructor(url = 'https://socialapp-api.herokuapp.com', client = axios.create()){
+    constructor(url = 'https://socialapp-api.herokuapp.com', client = axios.create()) {
         this.url = url;
         this.client = client;
     }
 
-    registerUser(registrationData){
+    registerUser(registrationData) {
         return this.client.post(this.url + "/users", registrationData);
+    }
+
+    listUsers() {
+        return this.client.get(this.url)
+      
+
     }
 
 }
--- src/pages/registrationForm/RegistrationForm.js
+++ src/pages/registrationForm/RegistrationForm.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Spinner from "react-spinkit";
+import { withRouter } from 'react-router-dom'
+
 // import { withAsyncAction } from "../../redux/HOCs";
 import "./RegistrationForm.css";
 import DataService from "../../dataService"
@@ -22,6 +24,9 @@ class RegistrationForm extends React.Component {
     this.client.registerUser(this.state).then(result => {
       // alert(JSON.stringify(result.data));
       alert('Success! Please login');
+      this.props.history.push('/')
+
+      // this.props.login(this.state)
       // this.setState({
       //   username: "",
       //   password: "",
@@ -74,5 +79,5 @@ class RegistrationForm extends React.Component {
   }
 }
 
-export default RegistrationForm;
+export default withRouter(RegistrationForm);
 // export default withAsyncAction("auth", "login")(LoginForm);
