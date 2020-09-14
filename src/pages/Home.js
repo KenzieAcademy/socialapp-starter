5e8e703c57aeec3531fa3f4a4eba5104154acaa4
Stacy Gilbert <StacyGilbert7713@gmail.com>
2020-09-14 16:24:45 -0400

    Let's Try Again

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,21 @@ import React from "react";
 import LoginForm from "../components/loginForm/LoginForm";
 import Menu from "../components/menu/Menu";
 import { userIsNotAuthenticated } from "../redux/HOCs";
+import { Link } from "react-router-dom";
 
 class Home extends React.Component {
   render() {
     return (
       <div className="Home">
         <Menu />
+<<<<<<< HEAD
         <h2>Your Favorite Microblogging Platform!</h2>
+=======
+        <h2>The hottest microblogging platform on the web</h2>
+>>>>>>> master
         <LoginForm />
+        <br />New user?<br />
+        <Link to="/registration">Register an account here!</Link>
       </div>
     );
   }
---
+++ src/pages/Registration.js
@@ -0,0 +1,21 @@
+import React from "react";
+import RegistrationForm from "../components/registrationForm/RegistrationForm";
+import Menu from "../components/menu/Menu";
+import { userIsNotAuthenticated } from "../redux/HOCs";
+import { Link } from "react-router-dom";
+
+class Registration extends React.Component {
+  render() {
+    return (
+      <div className="Registration">
+        <Menu />
+        <h2>Register a new account</h2>
+        <RegistrationForm />
+        <br />Already registered?<br />
+        <Link to="/">Login to your account here!</Link>
+      </div>
+    );
+  }
+}
+
+export default userIsNotAuthenticated(Registration);
