479f7ddc7493acab05665f94729c4a4012bb9787
Elise Cossairt <elise_cossairt@hotmail.comgit config --global user.name Eliserunclear--globalgit config user.email elise_cossairt@hotmail.com>
2020-09-15 15:40:31 -0500

    got registration page to clear form and link to login

--- src/components/registrationForm/RegistrationForm.js
+++ src/components/registrationForm/RegistrationForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Spinner from "react-spinkit";
 import { withAsyncAction } from "../../redux/HOCs";
 import "./RegistrationForm.css";
@@ -8,30 +9,35 @@ class RegistrationForm extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            username: "",
-            password: "",
-            displayName: ""
+            formData: {
+                username: "",
+                password: "",
+                displayName: "",
+            },
+            submitted: false
         };
         this.client = new DataService();
     }
 
     handleRegistration = e => {
         e.preventDefault();
-        this.client.registerUser(this.state).then(result => {
-            alert(JSON.stringify(result.data))
+        this.client.registerUser(this.state.formData).then(result => {
+            alert("User Is Registered")
         })
+        this.submittedForm()
     };
 
     handleChange = e => {
-        this.setState({ [e.target.name]: e.target.value });
-
-
+        let formData = this.state.formData
+        formData[e.target.name] = e.target.value
+        this.setState({ formData });
     };
-    resetForm = (event) => {
+    submittedForm() {
         this.setState({
             username: "",
             password: "",
-            displayName: ""
+            displayName: "",
+            submitted: true
         })
     }
 
@@ -39,39 +45,46 @@ class RegistrationForm extends React.Component {
 
     render() {
         const { loading, error } = this.props;
-        return (
-            <div className="RegistrationForm">
-                <form id="registration-form" onSubmit={this.handleRegistration}>
-                    <label htmlFor="username">Username</label>
-                    <input
-                        type="text"
-                        name="username"
-                        autoFocus
-                        required
-                        onChange={this.handleChange}
-                    />
-                    <label htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        name="password"
-                        required
-                        onChange={this.handleChange}
-                    />
-                    <label htmlFor="displayName">Display Name</label>
-                    <input
-                        type="text"
-                        name="displayName"
-                        required
-                        onChange={this.handleChange}
-                    />
-                    <button type="submit" disabled={loading}>
-                        Register
-          </button>
-                </form>
-                {loading && <Spinner name="circle" color="blue" />}
-                {error && <p style={{ color: "red" }}>{error.message}</p>}
-            </div>
-        );
+        if (this.state.submitted) {
+            return(
+                <Link to="/">Return to Login</Link>
+            )
+        }
+        else{
+            return (
+                <div className="RegistrationForm">
+                    <form id="registration-form" onSubmit={this.handleRegistration}>
+                        <label htmlFor="username">Username</label>
+                        <input
+                            type="text"
+                            name="username"
+                            autoFocus
+                            required
+                            onChange={this.handleChange}
+                        />
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            required
+                            onChange={this.handleChange}
+                        />
+                        <label htmlFor="displayName">Display Name</label>
+                        <input
+                            type="text"
+                            name="displayName"
+                            required
+                            onChange={this.handleChange}
+                        />
+                        <button type="submit" disabled={loading}>
+                            Register
+            </button>
+                    </form>
+                    {loading && <Spinner name="circle" color="blue" />}
+                    {error && <p style={{ color: "red" }}>{error.message}</p>}
+                </div>
+            );
+        }
     }
 }
 
