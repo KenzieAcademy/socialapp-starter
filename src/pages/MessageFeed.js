34c0c39bf1ad69cfce7f596861a6a919773c0894
Thorn <stachelrodtr4@gmail.com>
2020-09-15 19:00:48 -0400

    base message feed code

---
+++ src/pages/MessageFeed.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Menu from "../components/menu/Menu"
+
+class MessageFeed extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        // need to call the message list from the user's profile with a variable here for use in the render
+        return(
+            <div className="MessageFeed">
+                <Menu />
+                <p>sample text</p>
+            </div>
+        )
+    }
+}
+export default MessageFeed
