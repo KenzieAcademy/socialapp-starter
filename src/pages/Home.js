bcb19795170e06082b105be6ff385cceb3ae301e
Tammy Crittenden <tcritt71@gmail.com>
2020-09-15 10:23:44 -0500

    image added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,16 @@ import React from "react";
 import LoginForm from "../components/loginForm/LoginForm";
 import Menu from "../components/menu/Menu";
 import { userIsNotAuthenticated } from "../redux/HOCs";
+import cowboy1 from "../components/assets/images/cowboy1.jpg"
 
 class Home extends React.Component {
   render() {
     return (
       <div className="Home">
         <Menu />
-        <h2>Your favorite microblogging platform</h2>
+        <h2>Your favorite Convolooters Social App</h2>
         <LoginForm />
+        <img src={cowboy1} alt="cowboy1" />
       </div>
     );
   }
