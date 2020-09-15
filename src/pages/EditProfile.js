3bd48d4911973277efc291b462512928fa792f7b
Safea Patrick Mansa-Musa <safea.musa@gmail.com>
2020-09-15 11:28:42 -0400

    Safeas Addition 1st Contribution

---
+++ src/pages/EditProfile.js
@@ -0,0 +1,25 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+// // const { render } = require("react-dom");
+
+// render () {
+//     return
+// }
+// import React, { Component } from "react";
+// import { render } from "react-dom";
+
+// class EditProfile extends React.Component {
+//   render() {
+//     return (
+//       <div className="EditProfile">
+//         <form>
+//           <label htmlFor="EditProfile">Edit Profile</label>
+//           <input className="edit-name" type="input" name="displayname" />
+//           <button types="submit">Edit Profile</button>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
+// export default EditProfile;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,83 +1,51 @@
-import React,{Component} from 'react'
-import { render } from 'react-dom'
-import DataService from '../components/DataService'
+import React, { Component } from "react";
+import { render } from "react-dom";
+import DataService from "../components/DataService";
 
 class Register extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            
-                username: '',
-                password: '',
-                displayName:''
-            
-        }
-        this.client = new DataService()
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      displayName: "",
+    };
+    this.client = new DataService();
+  }
 
-    handleRegistration = e =>{
-        e.preventDefault()
-        this.client.registerUser(this.state).then(result =>{
-            alert(result.data)
-        })
-    }
+  handleRegistration = (e) => {
+    e.preventDefault();
+    this.client.registerUser(this.state).then((result) => {
+      alert(result.data);
+    });
+  };
 
-    handleChange = (event) => {
-        this.setState({[event.target.name]:event.target.value})
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-       
-    }
+  render() {
+    const { loading, error } = this.props;
+    return (
+      <div className="Register">
+        <form id="registration" onSubmit={this.handleRegistration}>
+          <label htmlFor="name"> User Name: </label>
 
-    
-    
-    render(){
-    const { loading, error} = this.props
-    return(
-        
-            <div className='Register'>
-                
-                <form id='registration' onSubmit={this.handleRegistration}>
-                    
-                   
-                        <label htmlFor='name'> User Name: </label>
-    
-                        <input 
-                        type='text' 
-                        name='username' 
-                        
-                        onChange={this.handleChange}
-                        />
-                    
+          <input type="text" name="username" onChange={this.handleChange} />
 
-                        
-                         <label htmlFor='password'> Password: </label>
+          <label htmlFor="password"> Password: </label>
 
-                        <input 
-                        type='password' 
-                        name='password' 
-                        
-                        onChange={this.handleChange}
-                        />
-                    
-                    
-                     
-                        <label htmlFor='displayName'> Display Name: </label>
-    
-                    <input 
-                        type='text' 
-                        name='displayName' 
-                        
-                        onChange={this.handleChange}
-                        />
-                    
+          <input type="password" name="password" onChange={this.handleChange} />
 
-                    <button type='submit'>Register </button>
-    
-                     
-                </form>
-                </div>
-        )
-    }
+          <label htmlFor="displayName"> Display Name: </label>
+
+          <input type="text" name="displayName" onChange={this.handleChange} />
+
+          <button type="submit">Register </button>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default Register
+export default Register;
