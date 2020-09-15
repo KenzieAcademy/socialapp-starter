e8a9e299af9a24251609edc741cdf06f01228d9a
Michael Reames <michael_reames@yahoo.com>
2020-09-15 11:50:38 -0400

    profile pic adding

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,9 @@
 import React from "react";
-import {Image, Upload, Button } from 'antd';
+import {Image, message, Upload, Button, Layout} from 'antd';
 import "antd/dist/antd.css"
 import Menu from "../components/menu/MenuAuthenticated";
 import { userIsAuthenticated } from "../redux/HOCs";
+
 import Icon from "@ant-design/icons/lib/components/Icon";
 
 class Profile extends React.Component {
@@ -14,16 +15,30 @@ class Profile extends React.Component {
   }
 
   render() {
+    const { Header, Content, Footer} = Layout;
     return (
-      <div className="Profile">
-        <Menu isAuthenticated={this.props.isAuthenticated} />
-        <h2>Your Profile Page</h2>
-        <Image></Image>
-        <Upload
-         name="file" customRequest={this.dummyRequest} accept=".png"
-        ><Button Icon type="Upload">Upload Profile picture here!</Button></Upload>
-        <button>Save picture</button>
-      </div>
+      <Layout className="site-layout" style={{ marginLeft: 190 }}>
+      <Header className="site-layout-background" style={{ padding: 0 }} />
+      <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+        <div className="site-layout-background" style={{ padding: 24, textAlign: 'center' }}>
+        <div className="Profile">
+      <Menu isAuthenticated={this.props.isAuthenticated} />
+       <h2>Your Profile Page</h2>
+        <hr/>
+      <Upload
+         name="avatar" listType="picture-card" className="avatar-uploader" 
+         accept=".png" >
+           <Button Icon type="Upload">Upload Profile picture here!</Button>
+           </Upload>
+        
+         <Button type="primary">Save picture</Button>
+    </div>
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
+    
+   
     );
   }
 }
