3a438b240c08bda019db416142916754387d8cca
Tammy Crittenden <tcritt71@gmail.com>
2020-09-15 17:40:21 -0500

    added 3 more images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,17 +2,21 @@ import React from "react";
 import LoginForm from "../components/loginForm/LoginForm";
 import Menu from "../components/menu/Menu";
 import { userIsNotAuthenticated } from "../redux/HOCs";
-import cowboy1 from "../components/assets/images/cowboy1.jpg"
+import cowboy1 from "../components/assets/images/cowboy1.png";
+import cowboy2 from "../components/assets/images/cowboy2.png";
+
 
 class Home extends React.Component {
   render() {
     return (
-      <div className="Home">
+      <div className="Home" >
         <Menu />
         <h2>Your favorite Convolooters Social App</h2>
         <LoginForm />
-        <img src={cowboy1} alt="cowboy1" />
-      </div>
+        <img className="cowboy1" src={cowboy1} alt="cowboy1" />
+
+        <img className="cowboy2" src={cowboy2} alt="cowboy2" />
+      </div >
     );
   }
 }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Menu from "../components/menu/Menu";
 import { userIsAuthenticated } from "../redux/HOCs";
+import cowboy3 from "../components/assets/images/cowboy3.png";
+import cowboy4 from "../components/assets/images/cowboy4.png";
+import cowgirl1 from "../components/assets/images/cowgirl1.png";
 
 class Profile extends React.Component {
   render() {
@@ -8,6 +11,9 @@ class Profile extends React.Component {
       <div className="Profile">
         <Menu isAuthenticated={this.props.isAuthenticated} />
         <h2>Profile</h2>
+        <img className="cowboy3" src={cowboy3} alt="cowboy3" />
+        <img className="cowboy4" src={cowboy4} alt="cowboy4" />
+        <img className="cowgirl1" src={cowgirl1} alt="cowgirl1" />
       </div>
     );
   }
