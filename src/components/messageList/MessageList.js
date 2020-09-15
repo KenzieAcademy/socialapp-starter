0e67ed0eeaa9d1c9672726b396f588edb3c21859
Doris Mondo <doris.ekwe@yahoo.com>
2020-09-15 13:36:23 -0400

    update

---
+++ src/components/messageList/MessageList.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import NewMessage from './NewMessage/NewMessages'
+import './MessageList.css'
+
+
+function MessageList(props) {
+    // const listItems = props.message.map((NewMessage, i) => <NewMessage key={i} NewMessage={NewMessage} />)
+    return (
+        <div>
+            <h1>Message List</h1>
+        </div>
+    )
+    //    <li key={i}>{NewMessage.received}</li>
+
+    //         <ul className="MessageList">
+    //             {listItems}
+    //         </ul>
+
+    //         )
+    // }
+
+    // <button onClick={() => this.props.deletemessage(this.props.id)}>Delete</button>
+
+    // this.setState((state, props) => ({
+
+
+}))
+
+
+export default MessageList
