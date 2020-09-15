c7ea7e7d0e5369ffb653ae5d82fba659c1b59720
James Farfan <jamesrojas710@gmail.com>
2020-09-15 15:44:30 -0400

    comment section in progress

--- src/pages/Messagefeed.js
+++ src/pages/Messagefeed.js
@@ -1,17 +1,34 @@
 import React, { Component } from "react";
+import { Layout } from 'antd';
+const { Sider, Content } = Layout;
 
 class Messagefeed extends Component {
+    constructor(props) {
+        super(props)
+        state = 
+        
+    }
+
     render() {
         return (
-            <div className="Messages">
-                <form className='feed'>
-                    <label>Create a Post</label>
-                    <input type="text" placeholder="Post here"></input>
-                    <buttton type="submit">Submit</buttton>
-                    <input id="addPost"></input>
+            <Layout>
 
-                </form>
-            </div>
+                <Sider>
+
+                    <div>
+                        <input type="text" placeholder="create a post"></input>
+                        <button>Post</button>
+                        <p></p>
+                    </div>
+
+                </Sider>
+
+                <Layout>
+                    <Content>Content</Content>
+                </Layout>
+
+            </Layout >
+
         );
     }
 }
