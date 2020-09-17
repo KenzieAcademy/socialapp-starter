320f9ca61031d2fc89122a54b94d41ca85fb9414
Chuefeng Vue <chuefe.vue@gmail.com>
2020-09-17 13:59:18 -0500

    subsitute commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import MainHub from "./pages/MainHub"
+import MainHub from "./pages/MainHub";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
-
 
 class App extends React.Component {
   render() {
     return (
       <Switch>
-        <Route 
+        <Route exact path="/" component={Home} />
+        <Route
           exact
-          path="/" 
-          component={Home} 
+          path="/profile/:username"
+          render={(props) => (
+            <Profile {...props} name={props.match.params.name} />
+          )}
         />
-        <Route 
-          exact 
-          path="/profile/:username" 
-          render = {(props) => <Profile {...props} name = {props.match.params.name}/> }  
-        />
-        <Route 
-          exact 
-          path="/mainHub" 
-          component = {MainHub} 
-        />
-        <Route 
-          exact 
-          path="*" 
-          component={NotFound} 
-        />
+        <Route exact path="/mainHub" component={MainHub} />
+        <Route exact path="*" component={NotFound} />
       </Switch>
     );
   }
