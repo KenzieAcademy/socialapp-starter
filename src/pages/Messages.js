dcde4b164555b022c253349cac0fd1f13dc57fd6
Sir Rodriguez <dr.raw.phd@gmail.com>
2020-09-17 12:12:00 -0400

    can render a message

--- src/pages/Messages.js
+++ src/pages/Messages.js
@@ -1,10 +1,37 @@
 import React, { Component } from "react";
+import MessageService from "../MessageService.js";
 
 class Messages extends Component {
+  constructor(props) {
+    super(props);
+    this.client = new MessageService();
+    this.state = {
+      data: []
+    };
+  }
+
+  getNewMessages = () => {
+    return this.client.getMessage().then((result) => {
+      let theMessage = result.data.messages 
+      this.setState({
+        data: theMessage[0]
+      })
+    })
+    
+  }
+
+  componentDidMount() {
+    this.getNewMessages();
+  }
+
   render() {
+
+
+
     return (
       <div className="MessagesDiv">
         <h1>Our Messages go on this page</h1>
+    <h2>Message: {this.state.data.text} </h2>
       </div>
     );
   }
