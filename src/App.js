0c8030a2f0f62739772590043b70a92756d101d9
Donovan Davis <dt-davis@Hotmail.com>
2020-09-17 11:23:03 -0500

    committing for merge

--- src/App.js
+++ src/App.js
@@ -4,26 +4,16 @@ import { Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
+import CreateUserDisplay from "./pages/CreateUserDisplay";
 
 class App extends React.Component {
   render() {
     return (
       <Switch>
-        <Route
-          exact
-          path="/"
-          component={Home}
-        />
-        <Route
-          exact
-          path="/profile/:username"
-          component={Profile}
-        />
-        <Route
-          exact
-          path="*"
-          component={NotFound}
-        />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/profile/:username" component={Profile} />
+        <Route exact path="/createUser" component={CreateUserDisplay} />
+        <Route exact path="*" component={NotFound} />
       </Switch>
     );
   }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import LoginForm from "../components/loginForm/LoginForm";
 import Menu from "../components/menu/Menu";
 import { userIsNotAuthenticated } from "../redux/HOCs";
+import { Link } from "react-router-dom";
 
 class Home extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class Home extends React.Component {
         <Menu />
         <h2>Your favorite microblogging platform</h2>
         <LoginForm />
+        <Link to="/createUser">Create User</Link>
       </div>
     );
   }
