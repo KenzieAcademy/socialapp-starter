ddf78ded8c3f93f42db8fe12f5d8143b830d60d3
cynthia cummins <cynthiacummins88@gmail.com>
2020-09-17 09:31:36 -0400

    mockup of profile

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -13,7 +13,7 @@ class Menu extends React.Component {
     return (
       <div className="Menu">
       
-        <h1>Kwitter</h1>
+        <h1>Book Nook and all things social</h1>
         {/* Line 17:9:  img elements must have an alt prop, either with meaningful text, 
             or an empty string for decorative images  jsx-a11y/alt-text
           eslint-disable-next-line to the line before.
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,6 +2,9 @@ import React from "react";
 import Menu from "../components/menu/Menu";
 import { userIsAuthenticated } from "../redux/HOCs";
 import FetchService from "../FetchService"
+import { Card, CardContent, Icon, Image } from 'semantic-ui-react'
+// import imageProfile from "../imageProfile/profile.jpg"
+
 
 class Profile extends React.Component {
   constructor(props) {
@@ -10,28 +13,28 @@ class Profile extends React.Component {
     this.client = new FetchService()
 
     this.state = {
-      formData:{
+      formData: {
         displayName: "",
         about: "",
         password: ""
       },
-  
-  "user": {
-    "pictureLocation": null,
-    "username": "null",
-    "displayName": "null",
-    "about": "null",
-    "googleId": null,
-    "createdAt": "null",
-    "updatedAt": "null",
 
-  },
-  "statusCode": null
-}
+      "user": {
+        "pictureLocation": null,
+        "username": "null",
+        "displayName": "null",
+        "about": "null",
+        "googleId": null,
+        "createdAt": "null",
+        "updatedAt": "null",
+
+      },
+      "statusCode": null
+    }
   }
 
-  componentDidMount(){
-    this.client.getUser(this.props.match.params.username).then((userData)=>{
+  componentDidMount() {
+    this.client.getUser(this.props.match.params.username).then((userData) => {
       this.setState(userData)
     })
   }
@@ -40,13 +43,13 @@ class Profile extends React.Component {
     const newformData = { ...this.state.formData };
     newformData[event.target.name] = event.target.value;
 
-    this.setState({ formData: newformData});
+    this.setState({ formData: newformData });
   };
 
   handleUpdateUser = event => {
     event.preventDefault()
     this.client.updateUser(this.state.user.username, this.state.formData)
-  
+
   }
 
   render() {
@@ -54,10 +57,25 @@ class Profile extends React.Component {
       <div className="Profile">
         <Menu isAuthenticated={this.props.isAuthenticated} />
         <h2>Profile</h2>
-        <h3>User Name: {this.state.user.username}</h3>
-        <h3>Display Name: {this.state.user.displayName}</h3>
-        <h3>About: {this.state.user.about}</h3>
-        <img/>
+        <Card> 
+          <Image src='https://images.pexels.com/photos/2846814/pexels-photo-2846814.jpeg?cs=srgb&dl=pexels-oziel-g%C3%B3mez-2846814.jpg&fm=jpg' wrapped ui={false} />
+
+          <Card.Content>
+            <Card.Header> {this.state.user.username}</Card.Header>
+            <Card.Description>{this.state.user.about}</Card.Description>
+
+            {/* <h3>User Name: {this.state.user.username}</h3> */}
+            {/* <h3>Display Name: {this.state.user.displayName}</h3> */}
+            {/* <h3>About: {this.state.user.about}</h3> */}
+            <img />
+          </Card.Content>
+
+          <Card.Content extra>
+            <a>
+              <Icon name='user' />
+            </a>
+          </Card.Content>
+        </Card>
 
         <form id="Profile-form" onSubmit={this.handleUpdateUser}>
           <label htmlFor="displayName">displayName</label>
