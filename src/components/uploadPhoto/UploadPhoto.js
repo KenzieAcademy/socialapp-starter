083858d2eb490552ef6fe20daa3cbb43a37e7755
foko53 <jenfoko@gmail.com>
2020-09-17 12:12:33 -0400

    profile photo set

--- src/components/uploadPhoto/UploadPhoto.js
+++ src/components/uploadPhoto/UploadPhoto.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 
-class UploadPhoto extends React.Component {
+class UploadPhoto extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: null,
+    };
+
+    this.onImageChange = this.onImageChange.bind(this);
+  }
+
+  onImageChange = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      let img = event.target.files[0];
+      this.setState({
+        image: URL.createObjectURL(img),
+      });
+    }
+  };
+
   render() {
     return (
-      <div className="UploadPhoto">
-        <button>Upload Photo</button>
+      <div>
+        <div>
+          <div>
+            <img src={this.state.image} />
+            <h1>Upload Image</h1>
+            <input type="file" name="myImage" onChange={this.onImageChange} />
+          </div>
+        </div>
       </div>
     );
   }
