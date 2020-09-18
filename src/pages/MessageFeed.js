3b84023dfa8f6eb36782732625122fd53a0761e5
3voker <Drechristophe@gmail.com>
2020-09-18 11:03:34 -0500

    Removed Message List replaced with MessageFeed.

--- src/pages/MessageFeed.js
+++ src/pages/MessageFeed.js
@@ -1,13 +1,40 @@
 import React from "react";
 import Menu from "../components/menu/Menu";
 import { userIsAuthenticated } from "../redux/HOCs";
+import api from "../socialappService";
+import Message from "../components/message/Message";
 
 class MessageFeed extends React.Component {
+  state = { messages: [] };
+
+  componentDidMount() {
+    api
+      .getMessages()
+      .then((response) =>
+        this.setState({ messages: response.data.messages }, () =>
+          console.log(response.data.messages)
+        )
+      );
+  }
+
   render() {
+    if (this.state.messages.length === 0) {
+      return (
+        <div className="MessageList">
+          <h1>MessageList</h1>
+          <h3>LOADING....</h3>
+        </div>
+      );
+    }
     return (
-      <div className="MessageFeed">
-        <Menu isAuthenticated={this.props.isAuthenticated} />
-        <h2>Messages</h2>
+      <div className="MessageList">
+        <Menu />
+        <h1>Message Feed</h1>
+        <ul>
+          {this.state.messages.map((messageObject) => {
+            return <Message {...messageObject} />;
+          })}
+        </ul>
       </div>
     );
   }
