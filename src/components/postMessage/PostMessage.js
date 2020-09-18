8477ec4ca6c56ad038c3078bf004a2c36c43a736
Sara Richardson <63122520+SaraRichardson129@users.noreply.github.com>
2020-09-18 17:14:01 -0500

    fixed merge conflict

--- src/components/postMessage/PostMessage.js
+++ src/components/postMessage/PostMessage.js
@@ -1,39 +1,16 @@
 import React from "react";
-
+import { Input } from "antd";
 
 function PostMessage(props) {
   return (
     <div className="messageForm">
       <form id="message-form" onSubmit={props.handlePostMessage}>
         <label htmlFor="text">Tell us what you're thinking!</label>
-        <input type="text" name="text" required onChange={props.handleChange} />
+        <Input type="text" name="text" required onChange={props.handleChange} />
         <button type="submit">Post</button>
       </form>
     </div>
   );
-
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-    console.log({ [e.target.name]: e.target.value });
-  };
-
-  render() {
-    return (
-      <div className="messageForm">
-        <form id="message-form" onSubmit={this.handleSubmit}>
-          <label htmlFor="text">Message</label>
-          <Input
-            type="text"
-            name="text"
-            autoFocus
-            required
-            onChange={this.handleChange}
-          />
-          <button type="submit">Post</button>
-        </form>
-      </div>
-    );
-  }
 }
 
 export default PostMessage;
