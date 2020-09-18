385c170997020e915c0111deb0368442512151b5
Chase Patrick <chasepatrick117@gmail.com>
2020-09-18 15:33:51 -0400

    fixed the moment function to display correct time of creation

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,6 +3,7 @@ import LikeButton from "../components/likebutton/LikeButton";
 import Menu from "../components/menu/Menu";
 import { userIsAuthenticated } from "../redux/HOCs";
 import DeleteMessage from "../components/deleteMessage/DeleteMessage";
+import DeleteAcctButton from "../components/deleteacct/DeleteAcct";
 //import ConfirmProp from "../components/comfimPrompt/ConfirmProp";
 
 class Profile extends React.Component {
@@ -11,14 +12,8 @@ class Profile extends React.Component {
       <div className="Profile">
         <Menu isAuthenticated={this.props.isAuthenticated} />
         <h2>Profile</h2>
-<<<<<<< HEAD
         <DeleteAcctButton />
         <DeleteMessage />
-=======
-        <UploadPhoto />
-        <Logout />
-        <LikeButton />
->>>>>>> setuserpic
       </div>
     );
   }
