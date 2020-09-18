0a902e433602ada75aac3c9990113db6f7f48737
rose <wittyrosegarcia@yahoo.com>
2020-09-18 11:39:40 -0400

    new added code

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
 class NotFound extends React.Component {
   render() {
     return (
       <div className="NotFound">
         <p>Page not found for {this.props.location.pathname}</p>
-        <Link to="/">Go Home</Link>
-      </div>
-    );
-  }
-}
+        
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSJIGn04fiHDa1R6xkCwBH8vlWsUEhlqYrXBg&usqp=CAU"alt=""></img>
 
+        <Link to="/">Go Home</Link>
+      </div> 
+    )    
+    
+    
+  }  
+}
 export default NotFound;
