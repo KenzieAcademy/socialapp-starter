37914d795ea562f8e3b734ad9233c39444b9f4d8
Chad <chdabney42@gmail.com>
2020-09-18 15:29:50 -0400

    added a post message component

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -2,12 +2,14 @@ import React from "react";
 import Menu from "../components/menu/Menu";
 import { userIsAuthenticated } from "../redux/HOCs";
 import MessageList from "../components/messageList/MessageList";
+import PostMessage from "../components/postMessage/PostMessage";
 
 class Feed extends React.Component {
   render() {
     return (
       <div className="Feed">
         <Menu isAuthenticated={this.props.isAuthenticated} />
+        <PostMessage />
         <MessageList />
       </div>
     );
