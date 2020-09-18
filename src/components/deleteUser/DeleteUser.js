44da929b9b7016eb5dd2eeab304dced7e9482dc6
william_michael_brough <eatmyloudmusic@gmail.com>
2020-09-17 21:53:31 -0400

    Installed Grommet

--- src/components/deleteUser/DeleteUser.js
+++ src/components/deleteUser/DeleteUser.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import DeleteUserService from "./DeleteUserService"
 // import { jsonHeaders, handleJsonResponse } from "../../redux/actionCreators/constants";
 // import axios from "axios"
+import { Grommet, Button1 } from 'grommet'
+
 
 class DeleteUser extends Component{
     constructor(props){
@@ -26,6 +28,7 @@ class DeleteUser extends Component{
 
 render() {
     return (
+        <Grommet>
         <div>
             <form id="delete-user" onSubmit={this.handleSubmit}>
             <label htmlFor="message">Type your login here: </label>
@@ -36,11 +39,15 @@ render() {
                     onChange={this.handleChange}
                 />
                 <br/>
+                <Button1
+                color='#00739D'>
                 <button type="submit">
                     Delete Forever
                 </button>
+                </Button1>
             </form>
         </div>
+         </Grommet>
     )
 }
 };
