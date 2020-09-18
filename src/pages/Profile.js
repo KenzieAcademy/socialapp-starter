86e561744dc14d407f59d0c7df066e960f543316
foko53 <jenfoko@gmail.com>
2020-09-18 17:32:22 -0400

    confirm delete account

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,8 +2,9 @@ import React from "react";
 import LikeButton from "../components/likebutton/LikeButton";
 import Menu from "../components/menu/Menu";
 import { userIsAuthenticated } from "../redux/HOCs";
-import Logout from "../components/logoutButton/LogoutButton";
+import UploadPhoto from "../components/uploadPhoto/UploadPhoto";
 import DeleteMessage from "../components/deleteMessage/DeleteMessage";
+import DeleteAcctButton from "../components/deleteacct/DeleteAcct";
 //import ConfirmProp from "../components/comfimPrompt/ConfirmProp";
 
 class Profile extends React.Component {
@@ -12,9 +13,10 @@ class Profile extends React.Component {
       <div className="Profile">
         <Menu isAuthenticated={this.props.isAuthenticated} />
         <h2>Profile</h2>
-        {/* <UploadPhoto /> */}
-        <Logout />
+        <UploadPhoto />
         <LikeButton />
+        <DeleteMessage />
+        <DeleteAcctButton />
       </div>
     );
   }
