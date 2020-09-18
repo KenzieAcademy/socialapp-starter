aaeb78fde1d76c0954e74c22d5d1d89492615bc0
Chuefeng Vue <chuefe.vue@gmail.com>
2020-09-18 14:53:53 -0500

    Feed message setup

--- src/components/messages/GetMessages.js
+++ src/components/messages/GetMessages.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Feed, Icon } from "semantic-ui-react";
 import MessageService from "../../services/MessageService";
 import Message from "./Message";
 
@@ -25,9 +26,11 @@ class GetMessages extends React.Component {
       <div className="messageList">
         <h1>Message List</h1>
         <ul>
-          {this.state.messages.map((messageObject) => (
-            <Message key={messageObject.id} {...messageObject} />
-          ))}
+          <Feed>
+            {this.state.messages.map((messageObject) => (
+              <Message key={messageObject.id} {...messageObject} />
+            ))}
+          </Feed>
         </ul>
       </div>
     );
