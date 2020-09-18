339edcca9cc9c7e30e412d3a5f10bf9b7fd1afe1
Sara Richardson <63122520+SaraRichardson129@users.noreply.github.com>
2020-09-18 14:40:53 -0500

    update

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import AddLikes from "../components/addLikes/AddLikes";
 import Menu from "../components/menu/Menu";
 import { userIsAuthenticated } from "../redux/HOCs";
 import MessageList from "../components/messageList/MessageList";
@@ -9,7 +8,6 @@ class Feed extends React.Component {
     return (
       <div className="Feed">
         <Menu isAuthenticated={this.props.isAuthenticated} />
-        {/* <AddLikes /> */}
         <MessageList />
       </div>
     );
