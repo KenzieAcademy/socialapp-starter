41567f80d681266e49c0e53a8f249b8df7a669c5
Donovan Davis <dt-davis@Hotmail.com>
2020-09-19 02:37:14 -0500

    fixed uncreated component beings called

--- src/components/newsFeed/NewsFeed.js
+++ src/components/newsFeed/NewsFeed.js
@@ -1,5 +1,4 @@
 import React from "react";
-import MessageCard from "../messageCard/MessageCard";
 
 class NewsFeed extends React.Component {
   constructor(props) {
@@ -10,15 +9,7 @@ class NewsFeed extends React.Component {
     return (
       <div className="NewsFeed">
         <h3>News Feed</h3>
-        <div className="NewsFeed-Message">
-          <MessageCard messageId={148} />
-          <MessageCard messageId={150} />
-          <MessageCard messageId={151} />
-          <MessageCard messageId={152} />
-          <MessageCard messageId={153} />
-        </div>
-        <br />
-        <br />
+        <div className="NewsFeed-Message">Message Card</div>
       </div>
     );
   }
