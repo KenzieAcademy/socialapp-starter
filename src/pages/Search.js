26f7e490d2893a17afd2277422aec9890607e812
Justin Caudill <jcaudill_2003@yahoo.com>
2020-09-19 08:44:26 -0400

    search page

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,9 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
-
-=======
 import { Link } from "react-router-dom";
->>>>>>> 86cd0263138da86a8919eb9949b53698e50d9c8e
 
 class Search extends React.Component {
   constructor(props) {
@@ -12,26 +8,12 @@ class Search extends React.Component {
   }
 
   handleChange = (e) => {
-<<<<<<< HEAD
-    e.preventDefault ()
-=======
     e.preventDefault();
->>>>>>> 86cd0263138da86a8919eb9949b53698e50d9c8e
     this.setState({ [e.target.name]: e.target.value });
   };
   render() {
     return (
       <div className="Search">
-<<<<<<< HEAD
-        <form>
-        <label htmlFor= "Search">Search</label>
-        <input
-          type="text"
-          placeholder="Search"
-          name="searchTerm"
-          onChange={this.handleChange}
-        />
-=======
         <Link to="/">Home</Link>
         <form>
           <label htmlFor="Search">Search</label>
@@ -41,7 +23,6 @@ class Search extends React.Component {
             name="searchTerm"
             onChange={this.handleChange}
           />
->>>>>>> 86cd0263138da86a8919eb9949b53698e50d9c8e
         </form>
         <ul>
           <li>Item 1</li>
@@ -50,13 +31,6 @@ class Search extends React.Component {
       </div>
     );
   }
-<<<<<<< HEAD
-
 }
 
 export default Search;
-=======
-}
-
-export default Search;
->>>>>>> 86cd0263138da86a8919eb9949b53698e50d9c8e
