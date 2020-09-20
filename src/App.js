1b16df7ef6f688d9665d3a20c84231e849eb7106
william_michael_brough <eatmyloudmusic@gmail.com>
2020-09-20 19:59:53 -0400

    commit -m 'deleteUser'

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import { Grommet } from 'grommet'
 
-
+import DeleteUser from "./pages/DeleteUser";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import MessageFeed from "./pages/MessageFeed";
@@ -32,6 +32,11 @@ class App extends React.Component {
           exact
           path="*"
           component={NotFound}
+        />
+        <Route
+          exact
+          path="/deleteUser"
+          component={DeleteUser}
         />
       </Switch>
       </Grommet>
