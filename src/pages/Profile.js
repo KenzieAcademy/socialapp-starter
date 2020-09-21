64849ebb39fcefcd893e92d542993fb3dd36dbde
TimothyTerry <62906968+TimothyTerry@users.noreply.github.com>
2020-09-21 14:10:08 -0400

    mmm

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,10 @@
 import React from "react";
 // import Menu from "../components/menu/Menu";
 import { userIsAuthenticated } from "../redux/HOCs";
+
  
 
-import { Layout, Carousel, Menu, Card, Col, Row, BackTop } from 'antd';
+import { Layout, Carousel, Card,Menu, Col, Row, BackTop } from 'antd';
 import {
   UpOutlined, 
   AppstoreOutlined,
@@ -71,7 +72,9 @@ const style = {
     return (
       <div className="Profile" style={{ height: '600vh', padding: 8 }}>
         <Menu isAuthenticated={this.props.isAuthenticated} />
+        
 
+      
 
        
         <Layout>
@@ -84,32 +87,7 @@ const style = {
             }}
           >
             <div className="logo" />
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-              <Menu.Item key="1" icon={<UserOutlined />}>
-                nav 1
-        </Menu.Item>
-              <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-                nav 2
-        </Menu.Item>
-              <Menu.Item key="3" icon={<UploadOutlined />}>
-                nav 3
-        </Menu.Item>
-              <Menu.Item key="4" icon={<BarChartOutlined />}>
-                nav 4
-        </Menu.Item>
-              <Menu.Item key="5" icon={<CloudOutlined />}>
-                nav 5
-        </Menu.Item>
-              <Menu.Item key="6" icon={<AppstoreOutlined />}>
-                nav 6
-        </Menu.Item>
-              <Menu.Item key="7" icon={<TeamOutlined />}>
-                nav 7
-        </Menu.Item>
-              <Menu.Item key="8" icon={<ShopOutlined />}>
-                nav 8
-        </Menu.Item>
-            </Menu>
+            
           </Sider>
           <Layout className="site-layout" style={{ marginLeft: 200 }}>
             <Header className="site-layout-background" style={{ padding: 0 }} />
@@ -277,6 +255,9 @@ const style = {
         <BackTop>
       <div style={style}><UpOutlined /></div>
     </BackTop>
+
+
+
       </div>
     );
   }
