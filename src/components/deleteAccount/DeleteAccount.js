cc39161b4f9adcf45fbcbd2f6dc004db6585bfd6
Andrew Brown <DaVinci0137@gmail.com>
2020-09-22 11:55:33 -0400

    added deleteAccount folder

---
+++ src/components/deleteAccount/DeleteAccount.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class DeleteAccount extends React.Component {
+  render() {
+    return (
+      <div className="DeleteAccount">
+        <button>Delete Account</button>
+      </div>
+    );
+  }
+}
+
+export default DeleteAccount;
