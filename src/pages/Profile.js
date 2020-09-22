ffd1447ba83aa026326e89714e421d290138f91c
Tammy Crittenden <tcritt71@gmail.com>
2020-09-22 10:02:12 -0500

    absolute to relative1

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Menu from "../components/menu/Menu";
-import Userspic from "../components/usersPic/UsersPic"
+import UsersPic from "../components/usersPic/UsersPic"
 import { userIsAuthenticated } from "../redux/HOCs";
 import cowboy3 from "../components/assets/images/cowboy3.png";
 import cowboy4 from "../components/assets/images/cowboy4.png";
@@ -12,7 +12,7 @@ class Profile extends React.Component {
     return (
       <div className="Profile">
         <Menu isAuthenticated={this.props.isAuthenticated} />
-        <Userspic />
+        <UsersPic />
         <h2>Profile</h2>
         <img className="cowboy3" src={cowboy3} alt="cowboy3" />
         <img className="cowboy4" src={cowboy4} alt="cowboy4" />
