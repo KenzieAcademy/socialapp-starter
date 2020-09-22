d0fe59f77e6f14c2b77608f75c62f7fa686ef277
James Farfan <jamesrojas710@gmail.com>
2020-09-22 12:21:03 -0400

    fixing merging errors

--- src/pages/Messagefeed.js
+++ src/pages/Messagefeed.js
@@ -17,78 +17,83 @@ class Messagefeed extends Component {
         super(props)
         this.state =
         {
+            data: {},
+            submitted: false,
             author: '',
             children: ''
         }
-
-
-
-    }
-    //Below here, the(function), will reflect a change to the VALUE inside, or of an element
-    //onChange attribute should contain this
-    commentHandler(event) {
-        this.setState({ comment: event.target.value });
-    }
-
-    authorHandler(event) {
-        this.setState({ author: event.target.value });
     }
 
 
-    handleSubmit = (event) => {
-        event.preventDefault();
-        this.setState({
-            submitted: true
-        })
-    }
+
+}
+//Below here, the(function), will reflect a change to the VALUE inside, or of an element
+//onChange attribute should contain this
+commentHandler = (event) => {
+    this.setState({ comment: event.target.value });
+}
+
+authorHandler = (event) => {
+    this.setState({ author: event.target.value });
+}
 
 
-    buttonHandler = (event) => {
-        console.log(event.value)
-    }
+submitHandler = (event) => {
+    event.preventDefault();
+    this.setState({
+        submitted: true
+    })
+}
 
-    render() {
+
+buttonHandler = (event) => {
+    console.log(event.value)
+}
+
+render() {
+    return (
+        <Layout>
+
+            <Header>
+                <div>
+                    <h1><span>The Dragon's Den</span></h1>
+                </div >
+            </Header>
 
 
-
-        return (
             <Layout>
 
-                <Header>
-                    <div className="">
-                        <h1><span>The Dragon's Den</span></h1>
-                    </div >
-                </Header>
+                <Content>
+                    <Profile />
+                    <div className="Home">
 
+                    </div>
+                </Content>
 
-                <Layout>
-
-                    <Content>
-                        <Profile />
-                    </Content>
-
-                    <Sider>
-                        <form  >
-                            <label>Comment Section</label>
-                            <CommentBox />
-                            <textarea row="1" cols="20"></textarea>
-                            <input onClick={this.buttonHandler} id="submitComment" type="button" value="post" ></input>
-                        </form>
-                    </Sider>
-                </Layout>
-
-                <Footer>
-                    <p>Facebook</p>
-                    <p>Instagram</p>
-                    <p>Twitter</p>
-                </Footer>
-
-
-
+                <Sider>
+                    <form  >
+                        <label>Comment Section</label>
+                        <CommentBox />
+                        <textarea row="1" cols="20"></textarea>
+                        <input onClick={this.buttonHandler} id="submitComment" type="button" value="post" ></input>
+                    </form>
+                </Sider>
             </Layout>
 
-        );
-    }
+            <Footer>
+                <p>Facebook</p>
+                <p>Instagram</p>
+                <p>Twitter</p>
+            </Footer>
+
+
+
+        </Layout>
+
+    );
 }
+
+
+
 
 export default userIsAuthenticated(Messagefeed);
