18530ff484684a1b0e5b03f82663bb86d7280a42
Darius <chapd289@robertmorris.edu>
2020-09-22 19:13:30 -0500

    messages

---
+++ src/pages/MessageFeed.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import Menu from '../components/menu/Menu'
+import Message from '../components/message/Message';
+import GetMessages from '../components/getMessages/GetMessages';
+import DataService from '../components/dataService';
+
+class MessageFeed extends React.Component {
+    state = { messages: [] }
+
+    componentDidMount() {
+        new DataService()
+            .getMessages()
+            .then(messages => {
+                this.setState({ messages })
+            })
+    }
+
+    render() {
+        if (this.state.messages.length === 0) {
+            return (
+                <div className="MessageFeed">
+                    <Menu />
+                    <GetMessages />
+                    <h1>Message Feed</h1>
+                    <h3>Loading...</h3>
+                </div>
+            )
+        }
+        
+        return (
+            <div className="MessageFeed">
+                <Menu />
+                <h1>Message Feed</h1>
+                <ul>
+                    {this.state.messages.map(msg => 
+                    <Message key={msg.id} {...msg} />)}
+                </ul>
+
+            </div>
+        )
+    }
+}
+
+export default MessageFeed
