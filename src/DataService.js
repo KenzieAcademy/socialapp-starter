dae579399e1ab0ff568f38be4827c9da5e66c7d2
Justin Caudill <jcaudill_2003@yahoo.com>
2020-09-24 17:31:11 -0400

    create message QA

--- src/DataService.js
+++ src/DataService.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 class DataService {
   constructor(
-    url = "https://socialapp-api.herokuapp.com",
+    url = "https://socialapp-api.herokuapp.com/",
     client = axios.create()
   ) {
     this.url = url;
@@ -12,7 +12,7 @@ class DataService {
   }
 
   registerUser(regisrationData) {
-    return this.client.post(this.url + "/users", regisrationData);
+    return this.client.post(this.url + "users", regisrationData);
   }
   getAllMessagesData() {
     return this.client.get(this.url + "messages");
