6b508fa4eaf7f92432710e7f5e2149ae7179fb8e
Mark Nelson <mnelson400@comcast.net>
2020-09-24 14:28:01 -0500

    Fixed Menu.js

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Menu.css";
 import { withAsyncAction } from "../../redux/HOCs";
-import Card from "react-bootstrap/Card";
+
 class Menu extends React.Component {
   constructor(props) {
     super(props);
@@ -20,22 +20,16 @@ class Menu extends React.Component {
 
   render() {
     return (
-
-      <div class="card" className="Menu">
-        <div class="card-header">
-          <h1>Clip-tastic! Home of extreme couponing!</h1>
-          {this.props.isAuthenticated && (
-            <div id="menu-links">
-              <Link to="/home">Home</Link>
-              <Link to="/profile">Profile</Link>
-              <Link to="/messagefeed">Message Feed</Link>
-              <Link to="/" onClick={this.handleLogout}>
-                Logout
-              </Link>
-            </div>
-          )}
-        </div>
-
+      <div className="Menu">
+        {this.props.isAuthenticated && (
+          <div id="menu-links">
+            <Link to={this.state.link}>PROFILE</Link>
+            <Link to="/messagefeed">MESSAGE FEED</Link>
+            <Link to="/" onClick={this.handleLogout}>
+              LOGOUT
+            </Link>
+          </div>
+        )}
       </div>
     );
   }
