2cb175ae24a75ddf57648291c41d83e04cdf41b1
Dervin White <dervin.white@gmail.com>
2020-09-24 13:41:42 -0400

    dervin4

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -8,6 +8,11 @@ import { Button } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 import { userIsAuthenticated } from "../redux/HOCs";
 
+
+
+
+
+
 class Profile extends React.Component {
   constructor(props) {
    
@@ -53,18 +58,22 @@ class Profile extends React.Component {
             height='200 px'
             width='200 px'
           />
-        </Segment>
-        <p> Display Name:  {"@" + this.state.user.displayName}</p>
-        <p> Useername:  {this.state.user.username}</p>
-        <p> About:  {this.state.user.about}</p>
-        <p> Profile created:  {this.state.user.createdAt}</p>
-        <p> Profile updated:  {this.state.user.updatedAt}</p>
+        </Segment> 
+        
+        
+        <p> <h2>Display Name:   ({"@" + this.state.user.displayName})</h2></p>
+        <p><h2> Username:  {this.state.user.username}</h2></p> 
+        <p><h2> About:  {this.state.user.about}</h2></p>
+        <p> <h2>Profile created:  {this.state.user.createdAt}</h2></p>
+        <p> <h2>Profile updated:  {this.state.user.updatedAt}</h2></p>
 
-        <Button content='Change Photo' primary
+        
+        
+        <Button color="orange" content ='Change Photo' primary />
+       
 
-        />
         <Link to={"/profile/updateprofile/" + this.props.match.params.username}>
-          <Button content='Update My Info' primary  />
+          <Button content  ='Update My Info' primary  />
         </Link>
 
 
@@ -75,3 +84,4 @@ class Profile extends React.Component {
 }
 
 export default userIsAuthenticated(Profile);
+
