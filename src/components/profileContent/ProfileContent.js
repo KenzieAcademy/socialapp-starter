cce0244273ad357b9cb9302f08e6725951d4cc8d
jeron harris <jeronharris13@gmail.com>
2020-09-24 13:42:34 -0500

    breaking

--- src/components/profileContent/ProfileContent.js
+++ src/components/profileContent/ProfileContent.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Image from 'react-bootstrap/Image'
-
+import ListGroup from 'react-bootstrap/ListGroup'
+import { userIsAuthenticated } from "../../redux/HOCs";
 
 class ProfileContent extends React.Component {
   render() {
@@ -9,11 +10,11 @@ class ProfileContent extends React.Component {
       <div className="ProfileContent">
         <Image>until</Image>
         <ListGroup>
+          <ListGroup.Item>display name</ListGroup.Item>
           <ListGroup.Item>bio</ListGroup.Item>
           <ListGroup.Item>location</ListGroup.Item>
-          <ListGroup.Item>joined date</ListGroup.Item>
           <ListGroup.Item>preferred brands</ListGroup.Item>
-          <ListGroup.Item>display name</ListGroup.Item>
+          <ListGroup.Item>joined date</ListGroup.Item>
       </ListGroup>
         <Button>hi</Button>
       </div>
