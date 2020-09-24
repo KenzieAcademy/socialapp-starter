97c3238232eeddc2ce4af8c5060aaf1c7ffa2ca5
damiean garciamendez <damieante@gmail.com>
2020-09-24 16:29:29 -0400

    daily save

--- src/components/messages/Messages.js
+++ src/components/messages/Messages.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React from "react";
 
 class Message extends React.Component {
-    render() { 
-        return ( 
-            <div>
+  render() {
+    return (
+      <div>
         <li className="messageList">
-        At {new Date(this.props.createdAt).toDateString()}
-        {this.props.username}
-        <div className="textDiv">{this.props.text}</div>
-        <div className="likesDiv">{this.props.likes.length}</div>
-        </li></div>
-         );
-    }
+          At {new Date(this.props.createdAt).toDateString()}
+          {this.props.username}
+          <div className="textDiv">{this.props.text}</div>
+          <div className="likesDiv">{this.props.likes.length}</div>
+        </li>
+      </div>
+    );
+  }
 }
- 
+
 export default Message;
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,20 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import Button from "@material-ui/core/Button";
+import "../components/assets/stylesheets/NotFound.css";
 
 class NotFound extends React.Component {
   render() {
     return (
-      <div className="NotFound">
-        <p>Page not found for {this.props.location.pathname}</p>
-        
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSJIGn04fiHDa1R6xkCwBH8vlWsUEhlqYrXBg&usqp=CAU"alt=""></img>
-
-        <Link to="/">Go Home</Link>
-      </div> 
-    )    
-    
-    
-  }  
+      <div className="container-NF">
+        <div className="NotFound">
+          <p>Page not found for {this.props.location.pathname}</p>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSJIGn04fiHDa1R6xkCwBH8vlWsUEhlqYrXBg&usqp=CAU"
+            alt="404 error"
+          />{" "}
+          <br />
+          <Link to="/">
+            <Button color="primary" type="submit">
+              <h3>Go Home</h3>
+            </Button>
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
+
 export default NotFound;
