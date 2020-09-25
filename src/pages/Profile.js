5b1e231f6f90e1992e635baa51fa132439313f44
Samuel Campbell <seebo321@comcast.net>
2020-09-25 13:44:49 -0500

    maybe

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,6 @@
 import React from "react";
 import Menu from "../components/menu/Menu";
 import { userIsAuthenticated } from "../redux/HOCs";
-
-class Profile extends React.Component {
-  render() {
-    return (
-      <div className="Profile">
-        <Menu isAuthenticated={this.props.isAuthenticated} />
-        <h2>Profile</h2>
 import UserData from "../components/getUsers/getUsers"
 import UserProfile from "../components/UpdateUser/UserProfile"
 class Profile extends React.Component {
