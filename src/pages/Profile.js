71b62ad2b832a56f7fd0bc6cfb705e5481756804
Ben Lynch <blynchisonfire@gmail.com>
2020-09-25 17:01:51 -0400

    beginning profile hookup

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,8 +3,35 @@ import Menu from "../components/menu/Menu";
 import { userIsAuthenticated } from "../redux/HOCs";
 import ProfileContent from "../components/profileContent/ProfileContent";
 import "../pages/Profile.css";
+import SocialappService from "../socialappService";
 
 class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.api = new SocialappService();
+    this.state = {
+      user: {},
+      password: "loading",
+      about: "loading",
+      displayName: "loading",
+    };
+  }
+
+  componentDidMount() {
+    this.api.getUser(localStorage.getItem("user")).then((response) =>
+      this.setState({
+        user: response.data.user,
+        password: response.data.user.password,
+        about: response.data.user.about,
+        displayName: response.data.user.displayName,
+      })
+    );
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
     return (
       <div className="Body">
