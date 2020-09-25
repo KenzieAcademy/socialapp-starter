d657809f1b5b6510ab065dba1d26c4f7053d7ab7
Courtney Sherrod <courtneylamontsherrod@gmail.com>
2020-09-25 14:24:32 -0500

    wq "added like functionlity"

--- src/components/message/Message.js
+++ src/components/message/Message.js
@@ -1,17 +1,26 @@
 import React from "react";
+import LikeService from "../../services/LikeService";
 // import userIsAuthenticated from "../../redux/HOCs"
 class Message extends React.Component {
+  likeHandler = (e) => {
+    new LikeService().createLike(this.props.id).then((response) => {
+      console.log(response);
+    });
+    console.log("like");
+  };
+
   render() {
     return (
       <li className="Message">
-        At {new Date(this.props.createdAt).toDateString()},
-        {this.props.username} posted:
+        At {new Date(this.props.createdAt).toDateString()},{this.props.username}{" "}
+        posted:
         <div className="message-text">{this.props.text}</div>
-        <div className="likes">Likes: {this.props.likes.length}</div>
+        <button className="likes" onClick={this.likeHandler}>
+          Likes: {this.props.likes.length}
+        </button>
       </li>
-    )
+    );
   }
 }
 
-
-export default Message
+export default Message;
