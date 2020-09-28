40a6b79cc9efecf46c4ef275510a86680156fda3
Lesa Ducksworth <lducks67@gmail.com>
2020-09-27 20:53:49 -0500

    not really sure

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,14 @@ import React from "react";
 import LoginForm from "../components/loginForm/LoginForm";
 import Menu from "../components/menu/Menu";
 import { userIsNotAuthenticated } from "../redux/HOCs";
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
+=======
+
+>>>>>>> Stashed changes
 import { Link } from "react-router-dom";
 
 class Home extends React.Component {
