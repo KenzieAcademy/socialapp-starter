d5def04d160d4cc19a633b19b7ca727416336476
mavis acheampong <amavis32@yahoo.com>
2020-09-29 17:23:07 -0400

    working delete account button

--- src/components/deleteAccount/DeleteAccount.js
+++ src/components/deleteAccount/DeleteAccount.js
@@ -3,28 +3,16 @@ import "./DeleteAccount.css";
 import DataService from "../../dataService";
 
 class DeleteAccount extends React.Component {
-<<<<<<< HEAD
-  handleDelete = (e) => {
-    e.preventDefault();
-    // new DataService().deleteUser().then(res => {
-    //   localStorage.clear();
-    //   window.location.reload();
-    // })
+  clickHandler = () => {
+    new DataService().deleteUser().then((res) => {
+      localStorage.clear();
+      window.location.reload();
+    });
   };
   render() {
     return (
       <div className="DeleteAccount">
-        <button onClick={this.handleDelete}>Delete Account</button>
-=======
-
-  clickHandler = () => {
-
-  }
-  render() {
-    return (
-      <div className="DeleteAccount">
         <button onClick={this.clickHandler}>Delete Account</button>
->>>>>>> 4b85910c4a36beeeb7b39b9fdbd9d3cc4e46bd06
       </div>
     );
   }
