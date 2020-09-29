02cabb1702e24f699c65267571e5b7f187d9985f
Tony Cain <r.tonycain@gmail.com>
2020-09-29 13:58:42 -0400

    Semantic added

--- src/pages/Message.js
+++ src/pages/Message.js
@@ -1,46 +1,42 @@
 import React from "react";
 import Menu from "../components/menu/Menu";
 import { userIsAuthenticated } from "../redux/HOCs";
-import { Form,Feed, FeedEvent, Input, Button, TextArea } from 'semantic-ui-react'
+import { Form, Feed, FeedEvent, Input, Button, TextArea } from 'semantic-ui-react'
 import DataService from '../components/DataService'
 
 class Messages extends React.Component {
 
-  
-constructor(props){
-  super(props)
 
-  this.state = {
-    text:""
-    
-      
-    
-     
-    
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      text: ""
+
+    }
+    this.client = new DataService()
   }
-  this.client = new DataService()
-}
-
-  
-handleChange = (event) => {
-  
-  this.setState({[event.target.name]:event.target.value})
-  
- 
-  
-}
-
-handleSubmit = (event) => {
- event.preventDefault();
- console.log(this.state)
- 
- 
 
 
- this.client.createMessage(this.state ).then(result =>{
-  console.log(result.data)
-})
-};
+  handleChange = (event) => {
+
+    this.setState({ [event.target.name]: event.target.value })
+
+
+
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state)
+
+
+
+
+    this.client.createMessage(this.state).then(result => {
+      console.log(result.data)
+    })
+  };
 
 
 
@@ -49,45 +45,45 @@ handleSubmit = (event) => {
 
 
 
-  
- 
+
+
 
   render() {
-    
-       
-      
-    return (
- 
-  <div className="Messages">
-    <Menu isAuthenticated={this.props.isAuthenticated} />
-    
-    <h2>Messages</h2>
-    
-   
-    <Form onSubmit={this.handleSubmit}>
-        <div>
 
-          <TextArea
-            placeholer='Enter Message'
-            style={{minHeight: 200, minWidth: 400}}
-            type="text"
-            name="text"
-            required
-            onChange={this.handleChange}
-          />
-          <br/>
-          <Button size="big" content="Post"/>
-         
-        </div>
-    
-    </Form>
-   
-    
-  </div>
-  
-     );
-    }
+
+
+    return (
+
+      <div className="Messages">
+        <Menu isAuthenticated={this.props.isAuthenticated} />
+
+        <h2>Messages</h2>
+
+
+        <Form onSubmit={this.handleSubmit}>
+          <div>
+
+            <TextArea
+              placeholer='Enter Message'
+              style={{ minHeight: 200, minWidth: 400 }}
+              type="text"
+              name="text"
+              required
+              onChange={this.handleChange}
+            />
+            <br />
+            <Button size="big" content="Post" />
+
+          </div>
+
+        </Form>
+
+
+      </div>
+
+    );
   }
+}
 
 
 export default userIsAuthenticated(Messages)
