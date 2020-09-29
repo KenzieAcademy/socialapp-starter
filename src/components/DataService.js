6982d0c22a3203d2d27561cabafc7d8de6527c4a
Tony Cain <r.tonycain@gmail.com>
2020-09-29 15:24:20 -0400

    Prettier

--- src/components/DataService.js
+++ src/components/DataService.js
@@ -1,71 +1,55 @@
-import axios from 'axios';
-
-
-
+import axios from "axios";
 
 class DataService {
-    constructor(url = 'https://socialapp-api.herokuapp.com', client = axios.create()) {
-        this.url = url;
-        this.client = client;
-        this.token = JSON.parse(localStorage.getItem('login'))
-    }
+  constructor(
+    url = "https://socialapp-api.herokuapp.com",
+    client = axios.create()
+  ) {
+    this.url = url;
+    this.client = client;
+    this.token = JSON.parse(localStorage.getItem("login"));
+  }
 
-    //users
+  //users
 
-    registerUser(userData) {
-        return this.client.post(this.url + "/users/", userData)
+  registerUser(userData) {
+    return this.client.post(this.url + "/users/", userData);
+  }
+  updateUser(userData, userName) {
+    return this.client.patch(this.url + `${"/users/"}${userName}`, userData, {
+      headers: {
+        Authorization: "Bearer " + this.token.result.token,
+      },
+    });
+  }
+  deleteUser(userName) {
+    return this.client.delete(this.url + "/users/" + userName, {
+      headers: {
+        Authorization: "Bearer " + this.token.result.token,
+      },
+    });
+  }
 
-    }
-    updateUser(userData, userName) {
-        return this.client.patch(this.url + `${"/users/"}${userName}`, userData, {
-            headers: {
+  //messages
 
-                Authorization: "Bearer " + this.token.result.token
-            }
-        })
+  createMessage(message) {
+    return this.client.post(this.url + "/messages", message, {
+      headers: {
+        Authorization: "Bearer " + this.token.result.token,
+      },
+    });
+  }
+  getMessages() {
+    return this.client.get(this.url + "/messages?limit=20").then((response) => {
+      return response.data.messages;
+    });
+  }
 
-    }
-    deleteUser(userName) {
-        return this.client.delete(this.url + '/users/' + userName, {
-            headers: {
-
-                Authorization: "Bearer " + this.token.result.token
-            }
-        })
-
-    }
-
-    //messages
-
-    createMessage(message) {
-        return this.client.post(this.url + '/messages', message, {
-            headers: {
-
-                Authorization: "Bearer " + this.token.result.token
-            }
-        })
-
-    }
-    getMessages() {
-        return this.client.get(this.url + '/messages?limit=20').then(response => { return response.data.messages})
-        
-
-    }
-
-
-
-    //likes
-    likeMessage() {
-        return this.client.post(this.url + '/messages').then (response => { console.log(response)})
-
-    }
-
-
-
-
-
-
-
-
+  //likes
+  likeMessage() {
+    return this.client.post(this.url + "/messages").then((response) => {
+      console.log(response);
+    });
+  }
 }
 export default DataService;
--- src/pages/Message.js
+++ src/pages/Message.js
@@ -1,106 +1,63 @@
 import React from "react";
 import Menu from "../components/menu/Menu";
 import { userIsAuthenticated } from "../redux/HOCs";
-import { Form, Button, TextArea, Feed } from 'semantic-ui-react'
-import DataService from '../components/DataService'
-import MessageFeed from '../components/messageFeed/MessageFeed'
+import { Form, Button, TextArea, Feed } from "semantic-ui-react";
+import DataService from "../components/DataService";
+import MessageFeed from "../components/messageFeed/MessageFeed";
 class Messages extends React.Component {
-
   constructor(props) {
-    super(props)
-
-
+    super(props);
 
     this.state = {
       text: "",
-
-
-
-
-
-
-    }
-    this.client = new DataService()
-
-
-
-
-  }
-
-
-
-  handleChange = (event) => {
-
-    this.setState({ [event.target.name]: event.target.value })
-
-
-
+    };
+    this.client = new DataService();
   }
 
   handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-    this.setState({ [event.target.name]: event.target.value })
-
-
-
-  }
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
   handleSubmit = (event) => {
     event.preventDefault();
-    console.log(this.state)
+    console.log(this.state);
 
-
-
-
-    this.client.createMessage(this.state).then(result => {
-      console.log(result.data)
-    })
+    this.client.createMessage(this.state).then((result) => {
+      console.log(result.data);
+    });
   };
 
+  render() {
+    return (
+      <div className="Messages">
+        <Menu isAuthenticated={this.props.isAuthenticated} />
 
-render() {
+        <h2>Messages</h2>
+        <ul>
+          <MessageFeed />
+        </ul>
 
-
-
-  return (
-
-    <div className="Messages">
-      <Menu isAuthenticated={this.props.isAuthenticated} />
-
-      <h2>Messages</h2>
-      <ul><MessageFeed /></ul>
-
-
-
-      <Form onSubmit={this.handleSubmit}>
-        <div>
-
-
-
-          <TextArea
-            placeholer='Enter Message'
-            style={{ minHeight: 200, minWidth: 400 }}
-            type="text"
-            name="text"
-
-            required
-            onChange={this.handleChange}
-          />
-          <br />
-          <Button size="big" content="Post" />
-
-        </div>
-
-      </Form>
-
-
-    </div>
-
-  );
+        <Form onSubmit={this.handleSubmit}>
+          <div>
+            <TextArea
+              placeholer="Enter Message"
+              style={{ minHeight: 200, minWidth: 400 }}
+              type="text"
+              name="text"
+              required
+              onChange={this.handleChange}
+            />
+            <br />
+            <Button size="big" content="Post" />
+          </div>
+        </Form>
+      </div>
+    );
   }
 }
-  
 
-
-
-export default userIsAuthenticated(Messages)
+export default userIsAuthenticated(Messages);
