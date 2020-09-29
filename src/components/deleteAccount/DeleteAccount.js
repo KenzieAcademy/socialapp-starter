7c7bc3d7c6488a0cec2f5dd139ab71e20be5e228
mavis acheampong <amavis32@yahoo.com>
2020-09-29 16:47:55 -0400

    update

--- src/components/deleteAccount/DeleteAccount.js
+++ src/components/deleteAccount/DeleteAccount.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./DeleteAccount.css";
+import DataService from "../../dataService";
 
 class DeleteAccount extends React.Component {
+  handleDelete = (e) => {
+    e.preventDefault();
+    // new DataService().deleteUser().then(res => {
+    //   localStorage.clear();
+    //   window.location.reload();
+    // })
+  };
   render() {
     return (
       <div className="DeleteAccount">
-        <button>Delete Account</button>
+        <button onClick={this.handleDelete}>Delete Account</button>
       </div>
     );
   }
