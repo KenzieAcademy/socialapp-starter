ef6014e2c3f7016c6dc3260a5cc62b4e1cc63665
Mark Nelson <mnelson400@comcast.net>
2020-09-30 14:03:52 -0500

    Updated CSS/Etc.

--- src/components/miniLogin/MiniLogin.js
+++ src/components/miniLogin/MiniLogin.js
@@ -23,7 +23,7 @@ class MiniLogin extends React.Component {
     const { loading, error } = this.props;
     return (
       <div className="PopUp">
-        <div className="EnterButton">
+        <div className="LoginButton">
           <Button
             variant="dark"
             size="lg"
