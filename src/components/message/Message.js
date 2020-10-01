385c1e788102655d8ba2a75746901168dbe566bb
Donovan Davis <dt-davis@Hotmail.com>
2020-10-01 14:56:44 -0500

    added state and props for rendering like and unlike buttons

--- src/components/message/Message.js
+++ src/components/message/Message.js
@@ -2,19 +2,49 @@ import React from "react";
 import GetDisplayName from "../getDisplayName/GetDisplayName";
 import LikeButton from "../likeButton/LikeButton";
 
-function Message(props) {
-  if (props.key) {
-    return <div>LOADING</div>;
+class Message extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { isLiked: false, likes: [] };
   }
-  return (
-    <li key={props.keyId} className="Message">
-      At {new Date(props.createdAt).toDateString()}{" "}
-      <GetDisplayName username={props.username} /> posted:
-      <div className="message-text">{props.text} </div>
-      <LikeButton messageId={props.id} />
-      <div className="likes">Likes: {props.likes.length}</div>
-    </li>
-  );
+
+  componentDidMount() {
+    let loginData = JSON.parse(localStorage.getItem("login"));
+    {
+      this.props.likes.forEach((likeObj) => {
+        if (likeObj.username === loginData.result.username) {
+          this.setState({ isLiked: true });
+        }
+      });
+    }
+  }
+  render() {
+    if (this.props.key) {
+      return <div>LOADING</div>;
+    }
+    if (!this.state.isLiked) {
+      return (
+        <li key={this.props.keyId} className="Message">
+          At {new Date(this.props.createdAt).toDateString()}{" "}
+          <GetDisplayName username={this.props.username} /> posted:
+          <div className="message-text">{this.props.text} </div>
+          <LikeButton messageId={this.props.id} />
+          <div className="likes">Likes: {this.props.likes.length}</div>
+        </li>
+      );
+    } else {
+      return (
+        <li key={this.props.keyId} className="Message">
+          At {new Date(this.props.createdAt).toDateString()}{" "}
+          <GetDisplayName username={this.props.username} /> posted:
+          <div className="message-text">{this.props.text} </div>
+          <button>unlike</button>
+          <div className="likes">Likes: {this.props.likes.length}</div>
+        </li>
+      );
+    }
+  }
 }
 
 export default Message;
