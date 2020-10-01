52197e742c83b0be3af563e7d15b39d241c715a3
Donovan Davis <dt-davis@Hotmail.com>
2020-10-01 15:36:30 -0500

    added rendering for delete message button and implimented delete message comoponent

--- src/components/message/Message.js
+++ src/components/message/Message.js
@@ -1,12 +1,12 @@
 import React from "react";
 import GetDisplayName from "../getDisplayName/GetDisplayName";
 import LikeButton from "../likeButton/LikeButton";
-
+import DeleteMessage from "../deleteMessage/DeleteMessage";
 class Message extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { isLiked: false, likes: [] };
+    this.state = { isLiked: false, likes: [], didCreate: false };
   }
 
   componentDidMount() {
@@ -18,31 +18,60 @@ class Message extends React.Component {
         }
       });
     }
+    if (this.props.username === loginData.result.username) {
+      this.setState({ didCreate: true });
+    }
   }
   render() {
     if (this.props.key) {
       return <div>LOADING</div>;
     }
-    if (!this.state.isLiked) {
-      return (
-        <li key={this.props.keyId} className="Message">
-          At {new Date(this.props.createdAt).toDateString()}{" "}
-          <GetDisplayName username={this.props.username} /> posted:
-          <div className="message-text">{this.props.text} </div>
-          <LikeButton messageId={this.props.id} />
-          <div className="likes">Likes: {this.props.likes.length}</div>
-        </li>
-      );
+    if (!this.state.didCreate) {
+      if (!this.state.isLiked) {
+        return (
+          <li key={this.props.keyId} className="Message">
+            At {new Date(this.props.createdAt).toDateString()}{" "}
+            <GetDisplayName username={this.props.username} /> posted:
+            <div className="message-text">{this.props.text} </div>
+            <LikeButton messageId={this.props.id} />
+            <div className="likes">Likes: {this.props.likes.length}</div>
+          </li>
+        );
+      } else {
+        return (
+          <li key={this.props.keyId} className="Message">
+            At {new Date(this.props.createdAt).toDateString()}{" "}
+            <GetDisplayName username={this.props.username} /> posted:
+            <div className="message-text">{this.props.text} </div>
+            <button>unlike</button>
+            <div className="likes">Likes: {this.props.likes.length}</div>
+          </li>
+        );
+      }
     } else {
-      return (
-        <li key={this.props.keyId} className="Message">
-          At {new Date(this.props.createdAt).toDateString()}{" "}
-          <GetDisplayName username={this.props.username} /> posted:
-          <div className="message-text">{this.props.text} </div>
-          <button>unlike</button>
-          <div className="likes">Likes: {this.props.likes.length}</div>
-        </li>
-      );
+      if (!this.state.isLiked) {
+        return (
+          <li key={this.props.keyId} className="Message">
+            At {new Date(this.props.createdAt).toDateString()}{" "}
+            <GetDisplayName username={this.props.username} /> posted:
+            <div className="message-text">{this.props.text} </div>
+            <LikeButton messageId={this.props.id} />
+            <div className="likes">Likes: {this.props.likes.length}</div>
+            <DeleteMessage messageId={this.props.id} />
+          </li>
+        );
+      } else {
+        return (
+          <li key={this.props.keyId} className="Message">
+            At {new Date(this.props.createdAt).toDateString()}{" "}
+            <GetDisplayName username={this.props.username} /> posted:
+            <div className="message-text">{this.props.text} </div>
+            <button>unlike</button>
+            <div className="likes">Likes: {this.props.likes.length}</div>
+            <DeleteMessage messageId={this.props.id} />
+          </li>
+        );
+      }
     }
   }
 }
