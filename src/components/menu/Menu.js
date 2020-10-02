d1f539afd38eb99f0bb26308da6d7f2ec9903378
Chazz Horn <Chazzhorn1@gmail.com>
2020-10-02 12:15:52 -0400

    Add edits to Nav bar

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -12,34 +12,32 @@ class Menu extends React.Component {
   render() {
     return (
       <div className="Menu">
-        <Layout className="layout">
-          <Layout.Header>
-            <div className="logo" />
-
-            <Menu2 theme="dark" mode="horizontal" defaultSelectedKeys={[""]}>
-              <Menu2.Item key="1">nav 1</Menu2.Item>
-              <Menu2.Item key="2">nav 2</Menu2.Item>
-              <Menu2.Item key="3">nav 3</Menu2.Item>
-            </Menu2>
-          </Layout.Header>
-        </Layout>
-
-        <h1>Observit</h1>
         {this.props.isAuthenticated && (
-          <div id="menu-links">
-            <Link to="/">Home</Link>
-
-            <Link
-              to={`/profile/${
-                JSON.parse(localStorage.getItem("login")).result.username
-              }`}
-            >
-              Profile
-            </Link>
-            <Link to="/" onClick={this.handleLogout}>
-              Logout
-            </Link>
-          </div>
+          <Layout className="layout">
+            <div className="logo" />
+            <Layout.Header>
+              <Menu2 theme="dark" mode="horizontal">
+                <Menu2.Item>
+                  {" "}
+                  <Link to="/">Home</Link>
+                </Menu2.Item>
+                <Menu2.Item>
+                  <Link
+                    to={`/profile/${
+                      JSON.parse(localStorage.getItem("login")).result.username
+                    }`}
+                  >
+                    Profile
+                  </Link>
+                </Menu2.Item>
+                <Menu2.Item>
+                  <Link to="/" onClick={this.handleLogout}>
+                    Logout
+                  </Link>
+                </Menu2.Item>
+              </Menu2>
+            </Layout.Header>
+          </Layout>
         )}
       </div>
     );
