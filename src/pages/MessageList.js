03223ad466545defa70d7f46912abcb2a18bd66d
Scott Martin <scooter@ianscottmartin@gmail.com>
2020-10-02 14:45:21 -0600

    fix

--- src/pages/MessageList.js
+++ src/pages/MessageList.js
@@ -1,74 +1,164 @@
+// // import React from "react";
+// // import Menu from "../components/menu/Menu";
+// // import api from "../services/BackendService";
+// // import Message from "../components/message/Message";
+// // import PostMessage from "../components/PostMessage";
+// // import GetUsersService from "../services/GetUsersService";
+
+// // class MessageList extends React.Component {
+// //     state = { messages: [], users: [] };
+
+
+// //     componentDidMount() {
+// //         api.getAllMessages().then((response) => {
+// //             this.setState({ messages: response.data.messages });
+// //         });
+// //         new GetUsersService().getUsers().then((response) => {
+// //             this.setState({ users: response.data.users });
+// //             console.log(response.data);
+// //         });
+// //     }
+// //     // handleMessagePost = (event) => {
+// //     //     event.preventDefault();
+// //     //     new PostMessage().postMessage({ text: this.state.text }).then((result) => {
+// //     //         console.log(result.data);
+// //     //     });
+// //     //     console.log("Post Button Pressed")
+// //     // };
+// //     handleChange = (event) => {
+// //         this.setState({
+// //             [event.target.name]: event.target.value,
+// //         });
+// //     };
+
+
+
+
+// //     render() {
+// //         if (this.state.messages.length === 0 || this.state.users.length === 0) {
+
+// //             return (
+// //                 <div className="MessageList">
+// //                     <Menu />
+// //                     <h1>MessageList</h1>
+// //                     <h3>LOADING...</h3>
+// //                 </div>
+// //             );
+// //         }
+// //         return (
+// //             <div className="MessageList">
+// //                 <Menu />
+// //                 <h1>Message Feed</h1>
+// //                 <PostMessage handleChange={this.handleChange} handleMessagePost={this.handleMessagePost} text={this.state.text} />
+// //                 <ul>
+// //                     {this.state.messages.map((messageObject) => (
+// //                         <Message key={messageObject.id} {...messageObject} />
+
+// //                     ))}
+// //                 </ul>
+
+// //                 <ul>
+// //                     <div className="Users">
+// //                         <h1> Users</h1>
+// //                         {this.state.users.map((userObject) => (
+// //                             <li>{userObject.username}</li>
+// //                         ))}
+// //                     </div>
+// //                 </ul>
+
+// //             </div>
+// //         );
+// //     }
+// // }
+
+// // export default MessageList;
 // import React from "react";
 // import Menu from "../components/menu/Menu";
-// import api from "../services/BackendService";
+// import Service from "../services/Service";
 // import Message from "../components/message/Message";
-// import PostMessage from "../components/PostMessage";
-// import GetUsersService from "../services/GetUsersService";
+// import PostMessage from "../components/postMessage/PostMessageForm"
+// import GetUsersService from "../services/GetUsersService"
+// import "./MessageList.css";
+// import MessageFeed from "./MessageFeed";
 
-// class MessageList extends React.Component {
-//     state = { messages: [], users: [] };
 
+// class MessageFeed extends React.Component {
+//     client = new Service();
+
+//     state = {
+//         message: [], users: [],
+//         text: "",
+//     };
 
 //     componentDidMount() {
-//         api.getAllMessages().then((response) => {
-//             this.setState({ messages: response.data.messages });
+//         this.client.getAllMessages().then((response) => {
+//             this.setState({ message: response.data.messages });
 //         });
 //         new GetUsersService().getUsers().then((response) => {
 //             this.setState({ users: response.data.users });
 //             console.log(response.data);
 //         });
 //     }
-//     // handleMessagePost = (event) => {
-//     //     event.preventDefault();
-//     //     new PostMessage().postMessage({ text: this.state.text }).then((result) => {
-//     //         console.log(result.data);
-//     //     });
-//     //     console.log("Post Button Pressed")
-//     // };
+
+//     handleMessagePost = (event) => {
+//         event.preventDefault();
+//         this.client.postMessage({ text: this.state.text })
+//             .then((result) => {
+//                 this.setState((currentState) => {
+//                     return {
+//                         message: [result.data.message, ...currentState.message],
+//                         text: ""
+//                     }
+//                 })
+//             });
+//         console.log("Post Button Pressed");
+//     };
+
 //     handleChange = (event) => {
 //         this.setState({
 //             [event.target.name]: event.target.value,
 //         });
 //     };
 
-
-
-
 //     render() {
-//         if (this.state.messages.length === 0 || this.state.users.length === 0) {
-
+//         if (this.state.message.length === 0 || this.state.users.length === 0) {
 //             return (
 //                 <div className="MessageList">
 //                     <Menu />
+                    
 //                     <h1>MessageList</h1>
 //                     <h3>LOADING...</h3>
 //                 </div>
 //             );
 //         }
+
 //         return (
 //             <div className="MessageList">
 //                 <Menu />
 //                 <h1>Message Feed</h1>
 //                 <PostMessage handleChange={this.handleChange} handleMessagePost={this.handleMessagePost} text={this.state.text} />
-//                 <ul>
-//                     {this.state.messages.map((messageObject) => (
-//                         <Message key={messageObject.id} {...messageObject} />
+//                 <div className="messageContainer">
+//                     <ul className="messageList">
+//                         {this.state.message.map((messageObject) => (
+//                             <Message key={messageObject.id} {...messageObject} />
+//                         ))}
+//                     </ul>
 
-//                     ))}
-//                 </ul>
 
-//                 <ul>
-//                     <div className="Users">
-//                         <h1> Users</h1>
+//                     <ul className="usersList">
+
+//                         <h1>Follow These Users</h1>
+
 //                         {this.state.users.map((userObject) => (
 //                             <li>{userObject.username}</li>
 //                         ))}
-//                     </div>
-//                 </ul>
 
+//                     </ul>
+//                 </div>
 //             </div>
+
 //         );
 //     }
 // }
 
-// export default MessageList;
+// export default MessageFeed;
