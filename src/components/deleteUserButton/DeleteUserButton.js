539df17d58a3d3d6ec3e536c1bd43ba1b09abba0
Justin Caudill <jcaudill_2003@yahoo.com>
2020-10-02 19:01:27 -0400

    trying to get delete user to work

--- src/components/deleteUserButton/DeleteUserButton.js
+++ src/components/deleteUserButton/DeleteUserButton.js
@@ -4,26 +4,44 @@ import { Popconfirm } from 'antd';
 
 
 class DeleteUserButton extends React.Component {
-  constructor() {
-    super();
-
+  constructor(props) {
+    super(props)
+    let loginData = JSON.parse(localStorage.getItem("login"));
+    
+    this.state = {
+      username: loginData.result.username,
+      isSubmitted: false,
+    }
     this.client = new DataService()
+
   }
+handleLoginRemoval = (e) => {
+  window.localStorage.removeItem('login')
+
+}
 
   handleDelete = (e) => {
     e.preventDefault();
-    this.client.deleteUser(this.state).then((result) => {
-      console.log(result.data);
+    this.client.deleteUser(this.state.username).then((result) => {
+      console.log(result);
+      this.setState ({isSubmitted: true})
     })
-      window.localStorage.removeItem('login')
-      window.location.reload()
     
+
+    window.location.reload()
   };
 
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+  // handleChange = (e) => {
+  //   this.setState({ [e.target.name]: e.target.value });
+  // }
   render() {
+    if (this.state.isSubmitted) {
+      return (
+        <button onClick={this.handleLoginRemoval}>
+          Login Page
+        </button>
+      )
+    }
     return <Popconfirm title="Are you sure？" okText="Yes" cancelText="No" onConfirm={this.handleDelete}>
       <button className="DeleteUserButton">Delete</button>;
   </Popconfirm>
