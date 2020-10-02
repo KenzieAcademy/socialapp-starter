849533bdf0c20c3238076e194cf79b17e12f8381
Courtney Sherrod <courtneylamontsherrod@gmail.com>
2020-10-02 15:14:16 -0500

    delete button completed

--- src/components/message/Message.js
+++ src/components/message/Message.js
@@ -1,7 +1,12 @@
 import React from "react";
 import LikeService from "../../services/LikeService";
+import DeleteMessageService from "../../services/DeleteMessageService";
 // import userIsAuthenticated from "../../redux/HOCs"
 class Message extends React.Component {
+  constructor(props) {
+    super(props);
+    this.client = new DeleteMessageService();
+  }
   likeHandler = (e) => {
     new LikeService().createLike(this.props.id).then((response) => {
       console.log(response);
@@ -9,7 +14,17 @@ class Message extends React.Component {
     console.log("like");
   };
 
+  handleDelete = (e) => {
+    this.client.createDelete(this.props.id).then((response) => {
+      console.log(response);
+    });
+  };
+
   render() {
+    let deleteButton;
+    if (this.props.username === this.client.getUserName()) {
+      deleteButton = <button onClick={this.handleDelete}>delete</button>;
+    }
     return (
       <li className="Message">
         At {new Date(this.props.createdAt).toDateString()},{this.props.username}{" "}
@@ -18,6 +33,7 @@ class Message extends React.Component {
         <button className="likes" onClick={this.likeHandler}>
           Likes: {this.props.likes.length}
         </button>
+        {deleteButton}
       </li>
     );
   }
