cc8ee5c2c3978f40876a6f1fe7ed24ace588e77d
Mark Nelson <mnelson400@comcast.net>
2020-10-02 16:26:07 -0500

    Repair Attempt

--- src/components/message/Message.js
+++ src/components/message/Message.js
@@ -1,22 +1,8 @@
 import React from "react";
-// import Image from "../../assets/images/Placeholder_Image.gif";
-// import Image from "react-bootstrap/Image";
 import Card from "react-bootstrap/Card";
-import { Link } from "react-router-dom";
 import "../message/Message.css";
 import ProfilePic from "../../assets/images/Placeholder_Image.gif";
-<<<<<<< HEAD
-<<<<<<< HEAD
-// import Image from "react-bootstrap/Image";
-// import { userIsAuthenticated } from "../../redux/HOCs";
-=======
 import SocialappService from "../../socialappService";
-import Image from "react-bootstrap/Image";
-=======
-import SocialappService from "../../socialappService";
->>>>>>> parent of a015c11... Merge branch 'master' into Mark
-import { userIsAuthenticated } from "../../redux/HOCs";
->>>>>>> parent of 7b5762f... Merge pull request #60 from BenLynch87/Ben
 
 class Message extends React.Component {
   constructor(props) {
@@ -26,10 +12,7 @@ class Message extends React.Component {
     this.state = {
       user: {},
       date: "",
-<<<<<<< HEAD
-=======
       userPic: ProfilePic,
->>>>>>> parent of a015c11... Merge branch 'master' into Mark
       likes: this.props.likes.length,
     };
   }
@@ -53,20 +36,12 @@ class Message extends React.Component {
   };
 
   render() {
-<<<<<<< HEAD
     let picture = ProfilePic;
     if (this.state.user.pictureLocation !== null) {
       picture = `https://socialapp-api.herokuapp.com${this.state.user.pictureLocation}`;
     }
 
     return (
-<<<<<<< HEAD
-=======
-    // if (this.state.userPic === "User does not have a picture") {
-    //   this.setState({ userPic: ProfilePic });
-    // }
-    return (
->>>>>>> parent of a015c11... Merge branch 'master' into Mark
       <div className="MessageBody">
         <div className="MessageCardBody">
           <Card className="MessageCard" style={{ width: "620px" }}>
@@ -76,25 +51,8 @@ class Message extends React.Component {
                 <Card.Title> Member: {this.props.username}</Card.Title>
               </div>
               {/* <div className="PostInfo"> */}
-<<<<<<< HEAD
               <Card.Subtitle className="mb-2 text-muted">
                 {new Date(this.props.createdAt).toDateString}{" "}
-=======
-      <div className="CardBody">
-        <Card style={{ width: "575px" }}>
-          <Card.Body className="Message">
-            <Image className="ProfilePic" src={picture} alt="Profile Pic" />
-            <div className="MemberTitle">
-              <Card.Title> Member: {this.state.user.displayName}</Card.Title>
-            </div>
-            <div className="PostedTitle">
-              <Card.Subtitle className="mb-2 text-muted">
-                {this.state.date}
->>>>>>> parent of 7b5762f... Merge pull request #60 from BenLynch87/Ben
-=======
-              <Card.Subtitle className="mb-2 text-muted">
-                {new Date(this.props.createdAt).toDateString}{" "}
->>>>>>> parent of a015c11... Merge branch 'master' into Mark
               </Card.Subtitle>
               {/* </div> */}
               <Card.Text className="MessageTextBox">
