34c4310fff80987b68d68205e52b2f7be490c1f0
barbara_workman <susan.workman90@gmail.com>
2020-10-04 14:54:43 -0400

    commits msgnavbar.css

--- src/components/MsgNavBar/MsgNavBar.js
+++ src/components/MsgNavBar/MsgNavBar.js
@@ -1,31 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { withAsyncAction } from "../../redux/HOCs";
+import "./MsgNavBar.css";
 
 class MsgNavBar extends React.Component {
-    handleLogout = (event) => {
-        event.preventDefault();
-        this.props.logout();
-    };
+  handleLogout = (event) => {
+    event.preventDefault();
+    this.props.logout();
+  };
 
-    render() {
-        return (
-            <div className="MsgNavBar">
-                <h1><span>The Dragon's Den</span></h1>
+  render() {
+    return (
+      <div className="MsgNavBar">
+        <h1>
+          <span>The Dragon's Den</span>
+        </h1>
 
-                {this.props.isAuthenticated && (
-                    <div id="menu-links">
-                        <Link to="/profile/:username">Profile</Link>
-                        <Link to="/messagefeed">Message Feed</Link>
-                        <Link to="/" onClick={this.handleLogout}>
-                            Logout
-                        </Link>
-                    </div>
-                )
-                }
-            </div >
-        );
-    }
+        {this.props.isAuthenticated && (
+          <div id="menu-links">
+            <Link to="/profile/:username">Profile</Link>
+            <Link to="/messagefeed">Message Feed</Link>
+            <Link to="/" onClick={this.handleLogout}>
+              Logout
+            </Link>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default withAsyncAction("auth", "logout")(MsgNavBar);
--- src/pages/Messagefeed.js
+++ src/pages/Messagefeed.js
@@ -1,107 +1,92 @@
 import React, { Component } from "react";
 import "../components/menu/Menu.css";
-import DataServices from '../dataService';
+import DataServices from "../dataService";
 import MsgNavBar from "../components/msgnavbar/MsgNavBar";
 import Comment from "../components/Comments/Comment";
 import { userIsAuthenticated } from "../redux/HOCs";
 
-import { Layout } from 'antd';
+import { Layout } from "antd";
 
 const { Header, Sider, Content, Footer } = Layout;
 
 class Messagefeed extends Component {
-    constructor(props) {
-        super(props)
-        this.client = new DataServices();
-        this.state =
-        {
-            messages: [],
-            data: {},
-            submitted: false,
-            author: '',
-            children: ''
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.client = new DataServices();
+    this.state = {
+      messages: [],
+      data: {},
+      submitted: false,
+      author: "",
+      children: "",
+    };
+  }
 
+  //Below here, the(function), will reflect a change to the VALUE inside, or of an element
+  //onChange attribute should contain this
+  componentDidMount = () => {
+    this.client.getMessages().then((data) => {
+      const list = data.data.messages;
+      this.setState({ ...this.state, messages: list }, () =>
+        console.log(this.state.messages)
+      );
+    });
+  };
 
+  submitHandler = (event) => {
+    event.preventDefault();
+    this.setState({
+      submitted: true,
+    });
+  };
+  handleChange = (event) => {
+    let comment = event.target.value;
+    console.log(comment);
+    this.setState({ children: comment });
+  };
 
+  buttonHandler = (event) => {
+    console.log(this.state.messages[0].text);
+  };
 
-    //Below here, the(function), will reflect a change to the VALUE inside, or of an element
-    //onChange attribute should contain this
-    componentDidMount = () => {
-        this.client.getMessages()
-            .then(data => {
-                const list = data.data.messages
-                this.setState({ ...this.state, messages: list }, () =>
-                    console.log(this.state.messages))
+  render() {
+    return (
+      <Layout className>
+        <MsgNavBar isAuthenticated={this.props.isAuthenticated} />
+        <Header>Hello</Header>
 
-            })
-    }
+        <Layout>
+          <Content>
+            <div className="Home">
+              {this.state.messages.map((msg) => (
+                <Comment key={msg.id} {...msg} />
+              ))}
+            </div>
+          </Content>
 
+          <Sider>
+            <form>
+              <label>Comment Section</label>
 
-    submitHandler = (event) => {
-        event.preventDefault();
-        this.setState({
-            submitted: true
-        })
-    }
-    handleChange = (event) => {
-        let comment = event.target.value;
-        console.log(comment)
-        this.setState({ children: comment });
-    }
-
-    buttonHandler = (event) => {
-        console.log(this.state.messages[0].text)
-    }
-
-    render() {
-        return (
-            <Layout className>
-                <MsgNavBar isAuthenticated={this.props.isAuthenticated} />
-                <Header>
-                    Hello
-
-
-                </Header>
-
-
-                <Layout>
-
-                    <Content>
-                        <div className="Home">
-                            {this.state.messages.map((msg) => (
-                                <Comment key={msg.id} {...msg} />
-
-                            ))}
-                        </div>
-                    </Content>
-
-                    <Sider>
-                        <form  >
-                            <label>Comment Section</label>
-
-
-                            <textarea onChange={this.handleChange} row="1" cols="20"></textarea>
-                            <button onClick={this.buttonHandler} id="submitComment" type="button" value="post" >Submit</button>
-                        </form>
-                    </Sider>
-                </Layout>
-
-                <Footer>
-                    <p>Facebook</p>
-                    <p>Instagram</p>
-                    <p>Twitter</p>
-                </Footer>
-
-
-
-            </Layout>
-
-        );
-    }
-
+              <textarea
+                onChange={this.handleChange}
+                row="1"
+                cols="20"
+              ></textarea>
+              <button
+                onClick={this.buttonHandler}
+                id="submitComment"
+                type="button"
+                value="post"
+              >
+                Submit
+              </button>
+            </form>
+          </Sider>
+        </Layout>
+      </Layout>
+    );
+  }
 }
-
 
 export default userIsAuthenticated(Messagefeed);
