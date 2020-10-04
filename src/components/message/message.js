01e503bbe5d0b5373c139616208c3f3485d5f0b5
Ashley <ashley_colleen@hotmail.com>
2020-10-04 18:26:27 -0400

    added code to message component

--- src/components/message/message.js
+++ src/components/message/message.js
@@ -1,3 +1,107 @@
+import React from "react";
+import SocialAppService from "../../socialAppService";
+
+class Message extends React.Component {
+
+  state = { likeCount: this.props.likes.length };
+  client = new SocialAppService();
+  handleLike = () => {
+    const loggedInUsername = this.client.getUsername();
+    const userHasLiked = this.props.likes.some(
+      (likeObject) => likeObject.username === loggedInUsername
+    );
+    if (userHasLiked) return;
+    this.client.postLike(this.props.id).then((like) => {
+      this.setState((latestState) => ({
+        likeCount: latestState.likeCount + 1,
+      }));
+    });
+  };
+  // handleDeleteLike = () => {
+  //   const loggedInUsername = this.client.getUsername();
+  // const userHasLiked = this.props.likes.find(
+  //     (likeObject) => likeObject.username === loggedInUsername
+  // )
+  // const doomedLike = this.props.likes.find(
+  //   (likeObject) => likeObject.username === loggedInUsername
+  // );
+  // if (doomedLike) return;
+  // handleDeleteLike = () => {
+  //     const loggedInUsername = this.client.getUsername();
+  //     const userHasLiked = this.props.likes.find(
+  //         (likeObject) => likeObject.like === loggedInUsername
+  //     )
+  // const doomedLike = this.props.likes.find(
+  //     (likeObject) => likeObject.username === loggedInUsername
+  // )
+  // if (userHasLiked) {
+  //     return
+  //     this.client.deleteLike(this.props.id).then((like) => {
+  //         this.setState((latestState) => ({
+  //             likeCount: latestState.likeCount - 1,
+  //         }))
+  //     })
+
+  // this.client.deleteLike(this.props.id).then((response) => {
+  // if (response.data.statusCode !== 200) return; 
+  // }
+  //         )
+  //         );
+  // });
+  // };
+  //    this.client.deleteLike(this.props.id).then((response) => {
+  //     if (response.data.statusCode !== 200) return;
+
+  //     this.setState((latestState) => ({
+  //       likeCount: latestState.likeCount - 1,
+  //     }));
+  //   });
+  // };
+  render() {
+    return (
+      <li className="message">
+        At {this.props.createdAt},{this.props.username}
+        posted
+        <div>
+          {this.props.text}
+          <div className="like-count">Likes: {this.state.likeCount}</div>
+          <button onClick={this.handleLike}>
+            <span role="img" aria-label="like">
+              👍
+            </span>
+          </button>
+          <br />
+          <button onClick={this.handleDeleteLike}>
+            <span role="img" aria-label="like">
+              👎{" "}
+            </span>
+          </button>
+        </div>
+      </li>
+    );
+  }
+}
+
+export default Message;
+
+//   .then(response => response.data.like)
+//   }
+// deleteLike(likeId) {
+//   const endpoint = `${this.url}/likes/`
+//   const config = {
+//     headers: {
+//       Authorization: "Bearer " + this.getToken(),
+//     },
+//   }
+//   return this.client.delete(endpoint, { likeId }, config)
+
+// }
+
+
+
+
+
+
 // import React from "react";
 // import { Button, Comment, Form, Header } from "semantic-ui-react";
 // import { withAsyncAction } from "../../redux/HOCs";
