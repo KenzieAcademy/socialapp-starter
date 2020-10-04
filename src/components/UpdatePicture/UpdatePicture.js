1b1875eadb4b8cc8ec70fdb9acef777da95e98d0
Samantha Taylor <staylor@mort.is>
2020-10-04 13:51:42 -0400

    Fixed all the issues we were having.

--- src/components/UpdatePicture/UpdatePicture.js
+++ src/components/UpdatePicture/UpdatePicture.js
@@ -2,69 +2,53 @@ import { WindowsFilled } from "@ant-design/icons"
 import { notification } from "antd"
 import React from "react"
 import QuestboardService from "../servicesPage/ServicePage"
+import ProfileImage from "../ProfileImage"
 
-const Questboard = new QuestboardService()
-const username = Questboard.getUsername()
 
-class FileUploader extends React.Component{
-    constructor (props) {
-        super (props)
+class FileUploader extends React.Component {
+    constructor(props) {
+        super(props)
+        this.Questboard = new QuestboardService();
+        const username = this.Questboard.getUsername();
+
         this.state = {
-        imageURL: `https://socialapp-api.herokuapp.com/users/${username}/picture`,
-        formData: null,}
-
-    
-}
-    setFallbackImage = event => {
-        event.target.src = "https://www.flaticon.com/free-icon/no-war_3456704"
+            imageURL: `https://socialapp-api.herokuapp.com/users/${username}/picture`,
+            formData: null,
+        }
     }
+
     createFormData = event => {
         const file = (event.target.files[0])
-        const formData = new FormData ()
+        const formData = new FormData()
         formData.append("picture", file)
-        this.setState({formData})
-        
+        this.setState({ formData })
     }
-    // componentDidUpdate(prevProps, prevState){
-    //     if (prevState.imageURL !== this.state.imageURL){
-    //         this.updatePicture()
-    //     }
-    //   }
-//         updatePicture = () => {
-//         const timestamp = Date.now()
-//         const imageURL = `https://www.socialapp-api.herokuapp.com/users/${username}/picture?t=${timestamp}`
-//         this.setState({imageURL})
-//         }
-// // 
+
     handleUpload = event => {
         event.preventDefault()
-        Questboard.SetPicture(this.state.formData)
-        .then( response => {
-            (notification.open({
-            message: "Upload successful!",
-            description: "Reloading page..."}))
-            // if (response.data.statusCode === 200) {
-            //     console.log("This works")
-            //     this.updatePicture()
-            // }
-            window.location.reload()
-        })
-        }
 
-    
+        this.Questboard.SetPicture(this.state.formData)
+            .then(response => {
+                const timestamp = Date.now()
+                this.setState({ imageURL: `${this.state.imageURL}?t=${timestamp}` })
+
+                notification.open({
+                    message: "Upload successful!",
+                })
+            })
+    }
+
     render() {
         return (
             <div className="FileUploader">
-                <input type="file" 
-                name="picture" 
-                onChange={this.createFormData}/>
+                <input type="file"
+                    name="picture"
+                    onChange={this.createFormData} />
                 <button onClick={this.handleUpload}>Upload</button>
                 <div className="image-preview">
-                    <img alt="user" src={this.state.imageURL}
-                    // onError={this.setFallbackImage}
-                    />
+                    <ProfileImage imageURL={this.state.imageURL} />
                 </div>
-                </div>
+            </div>
         )
     }
 
