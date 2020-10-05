e4d5894f1c41a14510477db9b8fb54d322be8c8e
Trinie J <jxsn00ks@gmail.com>
2020-10-05 08:16:00 -0400

    updated error with duplicate username in get a list of users

--- src/components/listUsers/ListUsers.js
+++ src/components/listUsers/ListUsers.js
@@ -1,40 +1,50 @@
 import React from "react";
-//import Menu from "../components/menu/Menu";
 import DataService from "../../services/dataService";
-import Users from "./Users";
+import Users from "../users/Users";
+import InfiniteScroll from "react-infinite-scroller";
 
-
-//min 4.19//
 class UserList extends React.Component {
-    state = { users: [] }
-    componentDidMount (){
+    state = { 
+        users: [],
+        hasMoreItems: true,
+        nextHref: null
+    };
+        componentDidMount() {
         new DataService().getUsers().then(users => {
-            this.setState({ users})
+            this.setState({ users })
         })
-
     }
 
-render (){
-if (this.state.users.length === 0){
-return(
-        <div className="ListOfUsers">
-           
-            <h1>Current Users</h1>
-            <h3>loading...</h3>
-        </div>
-)
-}
-return (
-        <div className="ListOfUsers">
-        <h1>this list</h1>
-            <ul>
-                {this.state.users.map(users => {
-               return <Users key= {users.username}{...users}/>
-                })}
-            </ul>
-          </div>
-    )
-}
+    render() {
+        if (this.state.users.length === 0) {
+            return (
+                <div className="ListOfUsers">
+
+                    <h1>Current Users</h1>
+                    <h3>loading...</h3>
+                </div>
+            )
+        }
+        return (
+            <div className="ListOfUsers">
+                <h1>Current List of Super Friends:</h1>
+                <ul>
+                    {this.state.users.map(users => {
+                        return <Users key={users.username}{...users} />
+                    })}
+                </ul>
+
+                <InfiniteScroll
+                    pageStart={0}
+                    loadMore={Users}
+                    hasMore={true || false}
+                    loader={<div className="loader" key={0}>Loading ...</div>}
+                >
+                    {Users}
+                </InfiniteScroll>
+            </div>
+        )
+    }
 }
 
 export default UserList;
