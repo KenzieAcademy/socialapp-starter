f9ddc5ccb1136e48cc4467873b6ac68910390a45
James Farfan <jamesrojas710@gmail.com>
2020-10-05 09:21:05 -0400

    messagefeeed update

--- src/pages/Messagefeed.js
+++ src/pages/Messagefeed.js
@@ -20,7 +20,7 @@ class Messagefeed extends Component {
         this.state =
         {
             messages: [],
-            data: {},
+            data: [],
             submitted: false,
             author: '',
             children: '',
@@ -65,20 +65,31 @@ class Messagefeed extends Component {
     }
 
     buttonHandler = (event) => {
+        this.setState({
+            data: {
+                author: this.state.author,
+                children: this.state.children,
+                date: this.state.date
+            }
+        })
         //console.log(this.state.messages[0].text)
-        console.log(this.state.author + ' posted at ' + this.state.date.toLocaleTimeString() + ' ' + this.state.children)
+        //console.log(this.state.author + ' posted at ' + this.state.date.toLocaleTimeString() + ' ' + this.state.children)
+
+
+        console.log(this.state.data)
 
     }
 
-    resetForm = (event) => {
+    /*resetForm = (event) => {
         this.setState({
             submitted: false,
             author: '',
             children: ''
         })
-    }
+    }*/
 
     render() {
+
 
 
 
@@ -97,7 +108,9 @@ class Messagefeed extends Component {
                 <Layout>
 
                     <Content>
+
                         <div className="Home">
+
                             {this.state.messages.map((msg) => (
                                 <Comment key={msg.id} {...msg} />
 
@@ -149,8 +162,7 @@ class Messagefeed extends Component {
 
 
                         </div>
-                        <Post author={this.state.author} date={this.state.date.toLocaleTimeString()} />
-                        <span>{this.state.author} {this.state.children}</span>
+                        <Post author={this.state.author} children={this.state.children} date={this.state.date.toLocaleTimeString()} />
                     </Sider>
                 </Layout>
 
@@ -167,128 +179,8 @@ class Messagefeed extends Component {
         );
     }
 
-=======
-  constructor(props) {
-    super(props);
-    this.client = new DataServices();
-    this.state = {
-      messages: [],
-      data: {},
-      submitted: false,
-      author: "",
-      children: "",
-    };
-  }
 
-  //Below here, the(function), will reflect a change to the VALUE inside, or of an element
-  //onChange attribute should contain this
-  componentDidMount = () => {
-    this.client.getMessages().then((data) => {
-      const list = data.data.messages;
-      this.setState({ ...this.state, messages: list }, () =>
-        console.log(this.state.messages)
-      );
-    });
-  };
-
-  submitHandler = (event) => {
-    event.preventDefault();
-    this.setState(
-      {
-        submitted: true,
-      } /*() => {
-            if (this.state.submitted) {
-                return this.state.messages.shift(...this.state.author)
-            }
-
-        }*/
-    );
-  };
-
-  authorChangehandler = (event) => {
-    let authorName = event.target.value;
-    this.setState({ author: authorName });
-  };
-
-  childrenChangehandler = (event) => {
-    let childrenText = event.target.value;
-    this.setState({ children: childrenText });
-  };
-
-  buttonHandler = (event) => {
-    //console.log(this.state.messages[0].text)
-    console.log(this.state.author);
-  };
-
-  render() {
-    return (
-      <Layout className>
-        <MsgNavBar isAuthenticated={this.props.isAuthenticated} />
-        <Header>
-          <div className="Header"></div>
-        </Header>
-
-        <Layout>
-          <Content>
-            <div className="Home">
-              {this.state.messages.map((msg) => (
-                <Comment key={msg.id} {...msg} />
-              ))}
-            </div>
-          </Content>
-
-          <Sider>
-            <div className="container">
-              <form onSubmit={this.submitHandler}>
-                <span>
-                  <label>Post Fury Comments</label>
-                </span>
-                <div>
-                  <input
-                    onChange={this.authorChangehandler}
-                    className="author"
-                    placeholder="Your name"
-                    type="text"
-                    name="author"
-                    value={this.state.author}
-                  />
-                </div>
-
-                <div>
-                  <textarea
-                    onChange={this.childrenChangehandler}
-                    placeholder="Post your thoughts"
-                    row="1"
-                    cols="16"
-                    name="children"
-                    value={this.state.children}
-                  ></textarea>
-                </div>
-
-                <button
-                  onClick={this.buttonHandler}
-                  id="submitComment"
-                  type="button"
-                >
-                  Submit
-                </button>
-              </form>
-            </div>
-
-            <span>
-              {this.state.author} {this.state.children}
-            </span>
-          </Sider>
-        </Layout>
-
-        <Footer>
-          <p>Facebook</p>
-          <p>Instagram</p>
-          <p>Twitter</p>
-        </Footer>
-      </Layout>
-    );
-  }
 }
+
 
 export default userIsAuthenticated(Messagefeed);
