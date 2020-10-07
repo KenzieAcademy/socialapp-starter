d5c959d89097bf7a06180c85c942db6a9bdcbb24
ADAM AMADI <ADAM.AMADI22@GMAIL.COM>
2020-10-06 21:26:42 -0500

    more editing

--- src/components/pagelayout/Header.js
+++ src/components/pagelayout/Header.js
@@ -7,22 +7,31 @@ const { Header, Footer, Sider, Content } = Layout;
 class TwitHeader extends React.Component {
   render() {
     return (
-      <Header style={{ padding: 13, backgroundColor: "dark" }}>
+      <Header style={{
+        padding: 100, backgroundImage:
+          "url(" +
+          "https://www.setaswall.com/wp-content/uploads/2017/03/City-Night-Raod-4K-Wallpaper-3840x2160.jpg" +
+          ")",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+
+      }}>
         <Title style={{ color: "lightblue" }} level={3}>
           <Avatar
-            style={{ float: "right" }}
-            size={48}
+            style={{ float: "left", left: "-95px", bottom: "1px" }}
+            size={100}
             src="https://cdn.dribbble.com/users/1525393/screenshots/6484130/comp_1.gif"
           />
-          <p
+          {/* <p
             style={{
               color: "lightblue",
-              fontFamily: "Brush Script MT",
+              fontFamily: "cursive",
               fontSize: "45px",
             }}
           >
             Twitter
-          </p>
+          </p> */}
         </Title>
       </Header>
     );
