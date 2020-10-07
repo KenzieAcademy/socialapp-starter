850e086f5bcad74add07a14332b9ed5c923d59e8
Chad <chdabney42@gmail.com>
2020-10-07 13:07:24 -0400

    Added more space around message feed wrapper.

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -6,10 +6,10 @@ import { BackTop, Layout } from "antd";
 
 const style = {
   height: 100,
-  width: 80,
+  width: 150,
   lineHeight: "40px",
   borderRadius: 4,
-  backgroundColor: "#e6e6ea",
+  backgroundColor: "#94b1af",
   color: "#060b1b",
   textAlign: "center",
   fontSize: 14,
@@ -20,12 +20,14 @@ const { Content, Footer } = Layout;
 class Feed extends React.Component {
   render() {
     return (
-      <Layout>
+      <Layout style={{ background: "#e6e6ea" }}>
         <div className="Feed">
           <Menu isAuthenticated={this.props.isAuthenticated} />
         </div>
 
-        <Content style={{ padding: "0 50px", background: "#060b1b" }}>
+        <Content
+          style={{ padding: "0 50px", background: "#060b1b", margin: "0 auto" }}
+        >
           <div
             className="site-layout-content"
             style={{ background: "#0a3051" }}
@@ -34,8 +36,8 @@ class Feed extends React.Component {
           </div>
         </Content>
 
-        <Footer style={{ textAlign: "center" }}>
-          Ant Design ©2018 Created by Ant UED
+        <Footer style={{ textAlign: "center", background: "#94b1af" }}>
+          Observit ©2020 Created by Team 404
         </Footer>
         <BackTop>
           <div style={style}>Back to top</div>
